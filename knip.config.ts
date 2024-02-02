f566c785006f6032fcbd1cd0760c4691252cd2b7
Farid Shabanov <farid.shabanov@binary-studio.com>
2024-02-02 15:10:47 +0400

    feat: add separate jobs for ci and ci-pr tm-1

--- knip.config.ts
+++ knip.config.ts
@@ -3,13 +3,13 @@ import { KnipConfig } from "knip";
 const config: KnipConfig = {
 	ignore: ["./prettier.config.ts", "./stylelint.config.ts", "./dangerfile.ts"],
 	ignoreDependencies: [
-		"pg",
 		"stylelint-config-recess-order",
 		"stylelint-config-standard",
 	],
 	workspaces: {
 		"apps/backend": {
 			entry: ["src/index.ts", "src/db/migrations/*.ts", "knexfile.ts"],
+			ignoreDependencies: ["pg"],
 		},
 		"apps/frontend": {
 			entry: ["src/index.tsx"],
