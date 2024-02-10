2e363733628435a877a332038506afc55f46900b
TeaChan <valkova.tanya17@gmail.com>
2024-02-11 00:35:59 +0200

    feat: add sign-in link to header component tm-20

--- apps/frontend/src/libs/components/header/header.tsx
+++ apps/frontend/src/libs/components/header/header.tsx
@@ -1,26 +1,32 @@
+import { NavLink } from "react-router-dom";
 import { Image } from "../image/image.js";
+import { AppRoute } from "~/libs/enums/app-route.enum.js";
 
 type Properties = {
 	user: {
 		image: {
 			url: string;
 		};
-	};
+	} | null;
 };
 
 const Header: React.FC<Properties> = ({ user }) => {
 	return (
 		<header>
 			<div>Search input</div>
-			<div>
-				<Image
-					src={user.image?.url}
-					alt="user-avatar"
-					width="48"
-					height="48"
-					isCircular
-				/>
-			</div>
+			{user ? (
+				<div>
+					<Image
+						src={user.image?.url}
+						alt="user-avatar"
+						width="48"
+						height="48"
+						isCircular
+					/>
+				</div>
+			) : (
+				<NavLink to={AppRoute.SIGN_IN}>Sign in</NavLink>
+			)}
 		</header>
 	);
 };
