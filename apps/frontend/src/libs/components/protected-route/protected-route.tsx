80ad0c30326d55c7268af04a8f63d239f7286273
Verhusha Oleg <olegverhusha@gmail.com>
2024-02-10 21:26:17 +0200

    chore: update import statements tm-15

--- apps/frontend/src/libs/components/protected-route/protected-route.tsx
+++ apps/frontend/src/libs/components/protected-route/protected-route.tsx
@@ -1,17 +1,15 @@
-import type { FC, ReactNode } from "react";
-
 import { Navigate } from "react-router-dom";
 
-import { AppRoute } from "~/libs/enums/app-route.enum.js";
+import { AppRoute } from "~/libs/enums/enums.js";
 import { useAppSelector } from "~/libs/hooks/hooks.js";
 import { type ValueOf } from "~/libs/types/types.js";
 
 type Properties = {
-	component: ReactNode;
+	component: React.ReactNode;
 	redirectTo?: ValueOf<typeof AppRoute>;
 };
 
-const ProtectedRoute: FC<Properties> = ({
+const ProtectedRoute: React.FC<Properties> = ({
 	component,
 	redirectTo = AppRoute.SIGN_IN,
 }) => {
