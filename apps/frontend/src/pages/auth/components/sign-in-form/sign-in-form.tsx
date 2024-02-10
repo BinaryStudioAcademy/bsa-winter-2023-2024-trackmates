d5ed3a90c2a492f4815ae99eba507040324c0213
Anna Maksai <annamaksai99@gmail.com>
2024-02-10 16:23:28 +0200

    feat: add 'Create an account' link to sing in page tm-10

--- apps/frontend/src/pages/auth/components/sign-in-form/sign-in-form.tsx
+++ apps/frontend/src/pages/auth/components/sign-in-form/sign-in-form.tsx
@@ -1,4 +1,6 @@
 import { Button } from "~/libs/components/components.js";
+import { Link } from "~/libs/components/components.js";
+import { AppRoute } from "~/libs/enums/app-route.enum.js";
 
 type Properties = {
 	onSubmit: () => void;
@@ -7,6 +9,9 @@ type Properties = {
 const SignInForm: React.FC<Properties> = () => (
 	<>
 		<h1>Log In</h1>
+		<p>
+			No account? Go to <Link to={AppRoute.SIGN_UP}>Create an account</Link>
+		</p>
 		<form>
 			<Button label="Sign in" />
 		</form>
