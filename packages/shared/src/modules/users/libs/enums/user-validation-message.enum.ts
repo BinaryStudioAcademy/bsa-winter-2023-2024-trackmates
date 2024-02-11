46bbd3e8b44e29271651f3d15ee66c190e37ca8f
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2024-02-11 20:43:50 +0200

    feat: validation schema tm-12

--- packages/shared/src/modules/users/libs/enums/user-validation-message.enum.ts
+++ packages/shared/src/modules/users/libs/enums/user-validation-message.enum.ts
@@ -1,6 +1,10 @@
 const UserValidationMessage = {
+	CONFIRM_PASSWORD_REQUIRE: "Confirm password is required",
 	EMAIL_REQUIRE: "Email is required",
 	EMAIL_WRONG: "Email is wrong",
+	FIRSTNAME_REQUIRE: "First name is required",
+	LASTNAME_REQUIRE: "First name is required",
+	PASSWORD_REQUIRE: "Password is required",
 } as const;
 
 export { UserValidationMessage };
--- packages/shared/src/modules/users/libs/validation-schemas/user-sign-up.validation-schema.ts
+++ packages/shared/src/modules/users/libs/validation-schemas/user-sign-up.validation-schema.ts
@@ -3,12 +3,21 @@ import { z } from "zod";
 import { UserValidationMessage, UserValidationRule } from "../enums/enums.js";
 
 type UserSignUpRequestValidationDto = {
+	confirmPassword: z.ZodString;
 	email: z.ZodString;
+	firstName: z.ZodString;
+	lastName: z.ZodString;
 	password: z.ZodString;
 };
 
 const userSignUp = z
 	.object<UserSignUpRequestValidationDto>({
+		confirmPassword: z
+			.string()
+			.trim()
+			.min(UserValidationRule.CONFIRM_PASSWORD_MINIMUM_LENGTH, {
+				message: UserValidationMessage.CONFIRM_PASSWORD_REQUIRE,
+			}),
 		email: z
 			.string()
 			.trim()
@@ -18,7 +27,24 @@ const userSignUp = z
 			.email({
 				message: UserValidationMessage.EMAIL_WRONG,
 			}),
-		password: z.string().trim(),
+		firstName: z
+			.string()
+			.trim()
+			.min(UserValidationRule.FIRSTNAME_MINIMUM_LENGTH, {
+				message: UserValidationMessage.FIRSTNAME_REQUIRE,
+			}),
+		lastName: z
+			.string()
+			.trim()
+			.min(UserValidationRule.LASTNAME_MINIMUM_LENGTH, {
+				message: UserValidationMessage.LASTNAME_REQUIRE,
+			}),
+		password: z
+			.string()
+			.trim()
+			.min(UserValidationRule.PASSWORD_MINIMUM_LENGTH, {
+				message: UserValidationMessage.PASSWORD_REQUIRE,
+			}),
 	})
 	.required();
 
