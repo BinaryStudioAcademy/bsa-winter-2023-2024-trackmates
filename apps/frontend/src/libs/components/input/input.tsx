9f45c3846b9861ab73ad174138c5910d7ab05a56
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2024-02-11 13:40:41 +0200

    feat: add styles for input tm-12

--- apps/frontend/src/libs/components/input/input.tsx
+++ apps/frontend/src/libs/components/input/input.tsx
@@ -7,18 +7,26 @@ import {
 
 import { useFormController } from "~/libs/hooks/hooks.js";
 
+import "./styles.css";
+
 type Properties<T extends FieldValues> = {
+	className?: string;
 	control: Control<T, null>;
 	errors: FieldErrors<T>;
+	isBasic?: boolean;
+	isPrimary?: boolean;
 	label: string;
 	name: FieldPath<T>;
 	placeholder?: string;
-	type?: "email" | "text";
+	type?: "email" | "password" | "text";
 };
 
 const Input = <T extends FieldValues>({
+	className,
 	control,
 	errors,
+	isBasic = false,
+	isPrimary = false,
 	label,
 	name,
 	placeholder = "",
@@ -29,11 +37,20 @@ const Input = <T extends FieldValues>({
 	const error = errors[name]?.message;
 	const hasError = Boolean(error);
 
+	const inputClasses = [
+		"input",
+		isPrimary && "primary-input",
+		isBasic && "basic-input",
+		className,
+	]
+		.filter(Boolean)
+		.join(" ");
+
 	return (
-		<label>
-			<span>{label}</span>
+		<label className={inputClasses}>
+			<span className="input__heading">{label}</span>
 			<input {...field} placeholder={placeholder} type={type} />
-			{hasError && <span>{error as string}</span>}
+			{hasError && <span className="error">{error as string}</span>}
 		</label>
 	);
 };
