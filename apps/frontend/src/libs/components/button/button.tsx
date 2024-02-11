52fbd77f7318ae90aa843154de566db5f259498f
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2024-02-11 13:27:57 +0200

    feat: add styles for button component tm-12

--- apps/frontend/src/libs/components/button/button.tsx
+++ apps/frontend/src/libs/components/button/button.tsx
@@ -1,11 +1,27 @@
+import "./styles.css";
+
 type Properties = {
+	className?: string;
+	isPrimary?: boolean;
 	label: string;
 	type?: "button" | "submit";
 };
 
 const Button: React.FC<Properties> = ({
+	className,
+	isPrimary = false,
 	label,
 	type = "button",
-}: Properties) => <button type={type}>{label}</button>;
+}: Properties) => {
+	const buttonClasses = ["button", isPrimary && "primary", className]
+		.filter(Boolean)
+		.join(" ");
+
+	return (
+		<button className={buttonClasses} type={type}>
+			{label}
+		</button>
+	);
+};
 
 export { Button };
