e02bf9a08b2abbc8d1ba987d198969204b92aa50
Maxim <toper.one111@gmail.com>
2024-02-12 16:41:34 +0200

    chore: add secret key from config to jwt module tm-14

--- apps/backend/src/libs/modules/token/base-token.module.ts
+++ apps/backend/src/libs/modules/token/base-token.module.ts
@@ -1,5 +1,6 @@
 import { SignJWT, jwtVerify } from "jose";
 
+import { config } from "../config/config.js";
 import { TokenPayload } from "./libs/types/token-payload.type.js";
 import { Token } from "./libs/types/types.js";
 
@@ -9,7 +10,7 @@ class BaseToken implements Token {
 		expiration: Date,
 	): Promise<string> {
 		// TODO: fix secret
-		const secret = new TextEncoder().encode("");
+		const secret = new TextEncoder().encode(config.ENV.JWT.SECRET_KEY);
 		const alg = "HS256";
 
 		return await new SignJWT(payload)
@@ -20,7 +21,7 @@ class BaseToken implements Token {
 
 	public async verifyToken(token: string): Promise<TokenPayload> {
 		// TODO: fix secret
-		const secret = new TextEncoder().encode("");
+		const secret = new TextEncoder().encode(config.ENV.JWT.SECRET_KEY);
 		const { payload } = await jwtVerify(token, secret);
 
 		return payload as TokenPayload;
