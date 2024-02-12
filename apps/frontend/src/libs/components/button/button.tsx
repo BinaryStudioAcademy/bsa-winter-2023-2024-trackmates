8411d0970445f0b98d4558684d9a5414aa985d68
Matvii <wantuchmatvii@gmail.com>
2024-02-12 19:34:48 +0200

    feat: add not found page styling tm-23

--- apps/frontend/src/libs/components/button/button.tsx
+++ apps/frontend/src/libs/components/button/button.tsx
@@ -1,11 +1,48 @@
+import clsx from "clsx";
+
+import { AppRoute } from "~/libs/enums/enums.js";
+import { type ValueOf } from "~/libs/types/types.js";
+
+import { Link } from "../link/link.js";
+import styles from "./styles.module.css";
+
 type Properties = {
+	color?: "primary";
+	href?: ValueOf<typeof AppRoute>;
 	label: string;
+	size?: "regular" | "small";
+	style?: "filled" | "outlined";
 	type?: "button" | "submit";
 };
 
 const Button: React.FC<Properties> = ({
+	color = "primary",
+	href,
 	label,
+	size = "regular",
+	style = "filled",
 	type = "button",
-}: Properties) => <button type={type}>{label}</button>;
+}: Properties) => {
+	const buttonStyles = clsx(
+		styles["button"],
+		styles[size],
+		styles[style],
+		styles[color],
+	);
+
+	return (
+		<>
+			{href ? (
+				<Link to={href}>
+					<div className={buttonStyles}>{label}</div>
+				</Link>
+			) : (
+				<button className={buttonStyles} type={type}>
+					{label}
+				</button>
+			)}
+		</>
+	);
+};
 
 export { Button };
--- apps/frontend/src/pages/not-found/not-found.tsx
+++ apps/frontend/src/pages/not-found/not-found.tsx
@@ -1,5 +1,39 @@
+import { Button } from "~/libs/components/components.js";
+
+import questionCharacter from "../../assets/img/question-character.svg";
+import classes from "./styles.module.css";
+
 const NotFound: React.FC = () => {
-	return <div></div>;
+	return (
+		<div className={classes["not-found-page-container"]}>
+			<div className={classes["not-found-page-content-container"]}>
+				<div className={classes["content-heading"]}>
+					<div className={classes["error-scene"]}>
+						<div className={classes["error-sign"]}>
+							<div
+								className={`${classes["error-sign-head"]} ${classes["octagon"]}`}
+							>
+								<div
+									className={`${classes["error-sign-content"]} ${classes["octagon"]}`}
+								>
+									<span className={classes["octagon"]}>404</span>
+								</div>
+							</div>
+							<div className={classes["error-sign-leg"]}></div>
+						</div>
+						<img alt="question character" src={questionCharacter} />
+					</div>
+				</div>
+				<h2 className={classes["title"]}>Page Not Found</h2>
+				<p className={classes["content"]}>
+					There is no page you are trying to find.
+				</p>
+				<div className={classes["actions"]}>
+					<Button href="/" label="Go Home" style="filled" />
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export { NotFound };
