86547a9c9baa7d299aff6c2994d0225ff81d492b
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2024-02-12 15:14:18 +0200

    feat: add new validation rules tm-12

--- packages/shared/src/modules/users/libs/enums/user-validation-message.enum.ts
+++ packages/shared/src/modules/users/libs/enums/user-validation-message.enum.ts
@@ -1,10 +1,27 @@
+import { UserValidationRule } from "./user-validation-rule.enum.js";
+
 const UserValidationMessage = {
 	CONFIRM_PASSWORD_REQUIRE: "Confirm password is required",
+	EMAIL_INVALID_FORMAT:
+		"Invalid email format. Please enter a valid email address.",
 	EMAIL_REQUIRE: "Email is required",
 	EMAIL_WRONG: "Email is wrong",
+	FIRSTNAME_INVALID_FORMAT:
+		"Invalid format for first name. Please use only letters, hyphens, and apostrophes.",
+	FIRSTNAME_MAX_LENGTH: `Username must be at most ${UserValidationRule.FIRSTNAME_MAX_LENGTH} characters long`,
+	FIRSTNAME_MIN_LENGTH: `Username must be at least ${UserValidationRule.FIRSTNAME_MINIMUM_LENGTH} characters long`,
 	FIRSTNAME_REQUIRE: "First name is required",
-	LASTNAME_REQUIRE: "First name is required",
+	LASTNAME_INVALID_FORMAT:
+		"Invalid format for last name. Please use only letters, hyphens, and apostrophes.",
+	LASTNAME_MAX_LENGTH: `Username must be at most ${UserValidationRule.LASTNAME_MAX_LENGTH} characters long`,
+	LASTNAME_MIN_LENGTH: `Username must be at least ${UserValidationRule.LASTNAME_MINIMUM_LENGTH} characters long`,
+	LASTNAME_REQUIRE: "Last name is required",
 	PASSWORD_REQUIRE: "Password is required",
+	PASSWORD_SHORT: `Password is too short (${UserValidationRule.PASSWORD_MINIMUM_LENGTH} characters at least)`,
+	PASSWORD_WEAK:
+		"Password must contain at least one uppercase and lowercase letter, one digit and one special character.",
+	PASSWORDS_NOT_MATCH:
+		"Passwords do not match. Please make sure your passwords match.",
 } as const;
 
 export { UserValidationMessage };
--- packages/shared/src/modules/users/libs/validation-schemas/user-sign-up.validation-schema.ts
+++ packages/shared/src/modules/users/libs/validation-schemas/user-sign-up.validation-schema.ts
@@ -15,7 +15,7 @@ const userSignUp = z
 		confirmPassword: z
 			.string()
 			.trim()
-			.min(UserValidationRule.CONFIRM_PASSWORD_MINIMUM_LENGTH, {
+			.min(UserValidationRule.PASSWORD_MINIMUM_LENGTH, {
 				message: UserValidationMessage.CONFIRM_PASSWORD_REQUIRE,
 			}),
 		email: z
@@ -26,26 +26,54 @@ const userSignUp = z
 			})
 			.email({
 				message: UserValidationMessage.EMAIL_WRONG,
-			}),
+			})
+			.regex(
+				/^(?=.{1,35}@)[\w!#$%&'*+/=?^`{|}~-]+(?:\.[\w!#$%&'*+/=?^`{|}~-]+)*@[\dA-Za-z-]+(?:\.[\dA-Za-z-]+)*$/,
+				{
+					message: UserValidationMessage.EMAIL_INVALID_FORMAT,
+				},
+			),
 		firstName: z
 			.string()
 			.trim()
 			.min(UserValidationRule.FIRSTNAME_MINIMUM_LENGTH, {
-				message: UserValidationMessage.FIRSTNAME_REQUIRE,
+				message: UserValidationMessage.FIRSTNAME_MIN_LENGTH,
+			})
+			.max(UserValidationRule.FIRSTNAME_MAX_LENGTH, {
+				message: UserValidationMessage.FIRSTNAME_MAX_LENGTH,
+			})
+			.regex(/^(?!['-])(?!.*['-].*['-])['A-Za-z-]{2,16}(?<!['-])$/, {
+				message: UserValidationMessage.FIRSTNAME_INVALID_FORMAT,
 			}),
 		lastName: z
 			.string()
 			.trim()
 			.min(UserValidationRule.LASTNAME_MINIMUM_LENGTH, {
-				message: UserValidationMessage.LASTNAME_REQUIRE,
+				message: UserValidationMessage.LASTNAME_MIN_LENGTH,
+			})
+			.max(UserValidationRule.LASTNAME_MAX_LENGTH, {
+				message: UserValidationMessage.LASTNAME_MAX_LENGTH,
+			})
+			.regex(/^(?!['-])(?!.*['-].*['-])['A-Za-z-]{3,25}(?<!['-])$/, {
+				message: UserValidationMessage.LASTNAME_INVALID_FORMAT,
 			}),
 		password: z
 			.string()
 			.trim()
 			.min(UserValidationRule.PASSWORD_MINIMUM_LENGTH, {
-				message: UserValidationMessage.PASSWORD_REQUIRE,
-			}),
+				message: UserValidationMessage.PASSWORD_SHORT,
+			})
+			.regex(
+				/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!"#$%&'()-_`{|}~])[\d!"#$%&'()-_`a-z{|}~]{8,}$/,
+				{
+					message: UserValidationMessage.PASSWORD_WEAK,
+				},
+			),
 	})
-	.required();
+	.required()
+	.refine((data) => data.password === data.confirmPassword, {
+		message: UserValidationMessage.PASSWORDS_NOT_MATCH,
+		path: ["confirmPassword"],
+	});
 
 export { userSignUp };
