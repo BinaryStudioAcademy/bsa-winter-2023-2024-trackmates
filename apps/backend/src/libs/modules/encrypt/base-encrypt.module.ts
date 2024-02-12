c21c68a40a41d0f66c83d451e9e0f8da0e7164c8
Kateryna Savelieva <krasilnikova.katerina@gmail.com>
2024-02-12 19:49:50 +0200

    fix: afrer review7 - extract SALT_ROUNDS to constants.ts tm-9

--- apps/backend/src/libs/modules/encrypt/base-encrypt.module.ts
+++ apps/backend/src/libs/modules/encrypt/base-encrypt.module.ts
@@ -1,8 +1,7 @@
 import { hash as genHash, genSalt } from "bcrypt";
 
+import { SALT_ROUNDS } from "./libs/constants/constants.js";
 import { Encrypt } from "./libs/types/types.js";
-
-const SALT_ROUNDS = 10;
 
 class BaseEncrypt implements Encrypt {
 	private static saltRounds = SALT_ROUNDS;
