88aa52dbcc2336d2df3b29a6a8ba0117c01925c2
TeaChan <valkova.tanya17@gmail.com>
2024-02-12 02:56:27 +0200

    feat: add button link variant and button styles tm-20

--- apps/frontend/src/libs/components/button/button.tsx
+++ apps/frontend/src/libs/components/button/button.tsx
@@ -1,11 +1,48 @@
+import { AppRoute } from "~/libs/enums/enums.js";
+import { type ValueOf } from "~/libs/types/types.js";
+
+import { Link } from "../components.js";
+import styles from "./styles.module.css";
+
 type Properties = {
+	color?: "primary";
+	href?: ValueOf<typeof AppRoute>;
+	isFluid?: boolean;
+	isSmall?: boolean;
 	label: string;
 	type?: "button" | "submit";
 };
 
 const Button: React.FC<Properties> = ({
+	color,
+	href,
+	isFluid,
+	isSmall,
 	label,
 	type = "button",
-}: Properties) => <button type={type}>{label}</button>;
+}: Properties) => {
+	const buttonStyles = [
+		styles["button"],
+		isSmall && styles["small"],
+		isFluid && styles["fluid"],
+		color ? styles[color] : styles["primary"],
+	]
+		.filter(Boolean)
+		.join(" ");
+
+	return (
+		<>
+			{href ? (
+				<Link to={href}>
+					<span className={buttonStyles}>{label}</span>
+				</Link>
+			) : (
+				<button className={buttonStyles} type={type}>
+					{label}
+				</button>
+			)}
+		</>
+	);
+};
 
 export { Button };
