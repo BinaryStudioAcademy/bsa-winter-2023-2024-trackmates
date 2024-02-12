405a541f1671243b765ae4765326d11a25eaa055
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2024-02-12 19:53:01 +0200

    feat: add design Signup tm-12

--- apps/frontend/src/pages/auth/components/sign-up-form/sign-up-form.tsx
+++ apps/frontend/src/pages/auth/components/sign-up-form/sign-up-form.tsx
@@ -1,6 +1,7 @@
-import logo from "~/assets/img/logo.png";
+import logo from "~/assets/img/svg/logo.svg";
 import { Button, Input, Link } from "~/libs/components/components.js";
 import { AppRoute } from "~/libs/enums/app-route.enum.js";
+import { getValidClassNames } from "~/libs/helpers/helpers.js";
 import { useAppForm, useCallback } from "~/libs/hooks/hooks.js";
 import {
 	type UserSignUpRequestDto,
@@ -8,7 +9,7 @@ import {
 } from "~/modules/users/users.js";
 
 import { DEFAULT_SIGN_UP_PAYLOAD } from "./libs/constants.js";
-import "./styles.css";
+import styles from "./styles.module.css";
 
 type Properties = {
 	onSubmit: (payload: UserSignUpRequestDto) => void;
@@ -28,64 +29,74 @@ const SignUpForm: React.FC<Properties> = ({ onSubmit }: Properties) => {
 	);
 
 	return (
-		<>
-			<main className="sign-up-page">
-				<div className="sign-up-page__container">
-					<div className="sign-up-page__logo">
-						<img alt="logo" src={logo} />
-						<span className="sign-up-page__logo-text">TrackMates</span>
+		<div className={styles["main-container"]}>
+			<div
+				className={getValidClassNames(
+					styles["form-container"],
+					styles["sign-in-form"],
+				)}
+			>
+				<h1 className={styles["logo-wrapper"]}>
+					{" "}
+					<img alt="Trackmates Logo" src={logo} />
+					TrackMates
+				</h1>
+
+				<form className={styles["form"]} onSubmit={handleFormSubmit}>
+					<div>
+						<h2 className={styles["title"]}>Create an account</h2>
+						<p className={styles["subtitle"]}>
+							Already have an account? Go to{" "}
+							<Link className={styles["link"]} to={AppRoute.SIGN_IN}>
+								{" "}
+								Log In
+							</Link>
+						</p>
 					</div>
-					<form className="sign-up-form" onSubmit={handleFormSubmit}>
-						<div>
-							<h2 className="sign-up-form__title">Create an account</h2>
-							<span className="sign-up-form__subtitle">
-								Already have an account? Go to
-								<span className="sign-up-form__link">
-									{" "}
-									<Link to={AppRoute.SIGN_IN}> Log In</Link>
-								</span>
-							</span>
-						</div>
-						<Input
-							control={control}
-							errors={errors}
-							label="First name"
-							name="firstName"
-							type="text"
-						/>
-						<Input
-							control={control}
-							errors={errors}
-							label="Last name"
-							name="lastName"
-							type="text"
-						/>
-						<Input
-							control={control}
-							errors={errors}
-							label="Email"
-							name="email"
-							type="text"
-						/>
-						<Input
-							control={control}
-							errors={errors}
-							label="Password"
-							name="password"
-							type="password"
-						/>
-						<Input
-							control={control}
-							errors={errors}
-							label="Confirm password"
-							name="confirmPassword"
-							type="password"
-						/>
-						<Button label="Create an account" type="submit" />
-					</form>
-				</div>
-			</main>
-		</>
+					<Input
+						color="dark"
+						control={control}
+						errors={errors}
+						label="First name"
+						name="firstName"
+						type="text"
+					/>
+					<Input
+						color="dark"
+						control={control}
+						errors={errors}
+						label="Last name"
+						name="lastName"
+						type="text"
+					/>
+					<Input
+						color="dark"
+						control={control}
+						errors={errors}
+						label="Email"
+						name="email"
+						type="text"
+					/>
+					<Input
+						color="dark"
+						control={control}
+						errors={errors}
+						label="Password"
+						name="password"
+						type="password"
+					></Input>
+					<Input
+						color="dark"
+						control={control}
+						errors={errors}
+						label="Confirm password"
+						name="confirmPassword"
+						type="password"
+					/>
+					<Button color="primary" label="Create an account" type="submit" />
+				</form>
+			</div>
+		</div>
 	);
 };
 
