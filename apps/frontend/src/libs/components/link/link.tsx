b206a2dd5e672c20ff311a8d06c1ec7c2ad830d6
TeaChan <valkova.tanya17@gmail.com>
2024-02-12 02:31:57 +0200

    feat: add styles to link component tm-20

--- apps/frontend/src/libs/components/link/link.tsx
+++ apps/frontend/src/libs/components/link/link.tsx
@@ -3,13 +3,17 @@ import { NavLink } from "react-router-dom";
 import { type AppRoute } from "~/libs/enums/enums.js";
 import { type ValueOf } from "~/libs/types/types.js";
 
+import styles from "./styles.module.css";
+
 type Properties = {
 	children: React.ReactNode;
 	to: ValueOf<typeof AppRoute>;
 };
 
 const Link: React.FC<Properties> = ({ children, to }: Properties) => (
-	<NavLink to={to}>{children}</NavLink>
+	<NavLink className={styles["link"] as string} to={to}>
+		{children}
+	</NavLink>
 );
 
 export { Link };
