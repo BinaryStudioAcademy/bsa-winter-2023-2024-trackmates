27454e9773e7734d8468ddc26057ce33592613d7
Kateryna Savelieva <krasilnikova.katerina@gmail.com>
2024-02-12 11:08:15 +0200

    fix: afrer review4 - BaseEncrypt. add static property saltRounds  tm-9

--- apps/backend/src/libs/modules/encrypt/base-encrypt.module.ts
+++ apps/backend/src/libs/modules/encrypt/base-encrypt.module.ts
@@ -5,6 +5,8 @@ import { Encrypt } from "./libs/types/types.js";
 const SALT_ROUNDS = 10;
 
 class BaseEncrypt implements Encrypt {
+	private static saltRounds = SALT_ROUNDS;
+
 	public async compare(
 		data: string,
 		hash: string,
@@ -16,7 +18,7 @@ class BaseEncrypt implements Encrypt {
 	}
 
 	public async encrypt(data: string): ReturnType<Encrypt["encrypt"]> {
-		const salt = await genSalt(SALT_ROUNDS);
+		const salt = await genSalt(BaseEncrypt.saltRounds);
 		const hash = await genHash(data, salt);
 
 		return { hash, salt };
