130c9815c773d4c9a3d968423542e8ff3005e61c
Maxim <toper.one111@gmail.com>
2024-02-13 17:15:23 +0200

    chore: change fn declarations to fn expressions tm-14

--- apps/backend/src/libs/modules/plugins/libs/helpers/get-api-endpoint.helper.ts
+++ apps/backend/src/libs/modules/plugins/libs/helpers/get-api-endpoint.helper.ts
@@ -1,10 +1,10 @@
-function getApiEndpoint(url: string): null | string | undefined {
+const getApiEndpoint = (url: string): null | string | undefined => {
 	const regex = /\/api\/v\d+(\/.+)/;
 	const match = url.match(regex);
 
 	const [, capturedValue] = match ?? [];
 
 	return capturedValue ?? null;
-}
+};
 
 export { getApiEndpoint };
--- apps/backend/src/libs/modules/plugins/libs/helpers/is-white-route.helper.ts
+++ apps/backend/src/libs/modules/plugins/libs/helpers/is-white-route.helper.ts
@@ -2,10 +2,10 @@ import { WHITE_ROUTES } from "~/libs/modules/config/libs/constants/constants.js"
 
 import { getApiEndpoint } from "./get-api-endpoint.helper.js";
 
-function isWhiteRoute(url: string) {
+const isWhiteRoute = (url: string) => {
 	const apiEndpoint = getApiEndpoint(url);
 
 	return WHITE_ROUTES.includes(apiEndpoint ?? "");
-}
+};
 
 export { isWhiteRoute };
