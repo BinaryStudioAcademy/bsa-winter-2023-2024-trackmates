d8efc79dad25603cb02ea5439d21426d7ecb8298
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2024-02-13 13:47:00 +0200

    fix: update import path tm-12

--- apps/frontend/src/pages/auth/components/sign-up-form/sign-up-form.tsx
+++ apps/frontend/src/pages/auth/components/sign-up-form/sign-up-form.tsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import logo from "~/assets/img/svg/logo.svg";
 import { Button, Input, Link } from "~/libs/components/components.js";
-import { AppRoute } from "~/libs/enums/app-route.enum.js";
+import { AppRoute } from "~/libs/enums/enums.js";
 import { getValidClassNames } from "~/libs/helpers/helpers.js";
 import { useAppForm, useCallback, useState } from "~/libs/hooks/hooks.js";
 import {
