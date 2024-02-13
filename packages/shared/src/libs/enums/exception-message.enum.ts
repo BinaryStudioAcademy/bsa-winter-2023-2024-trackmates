d2f6bdde0293652006e35c15c3bb75fb020fc7f4
Maxim <toper.one111@gmail.com>
2024-02-13 19:49:33 +0200

    chore: remove unused exception messages tm-14

--- packages/shared/src/libs/enums/exception-message.enum.ts
+++ packages/shared/src/libs/enums/exception-message.enum.ts
@@ -2,8 +2,6 @@ const ExceptionMessage = {
 	EMAIL_ALREADY_EXISTS: "Email is already taken.",
 	INCORRECT_CREDENTIALS: "Incorrect credentials.",
 	INVALID_TOKEN: "Token is invalid.",
-	NO_AUTH_HEADER: "Authorization header is required",
-	NO_JWT: "JWT is required",
 	NO_USER: "User not found",
 	UNKNOWN_ERROR: "Unknown error occurred.",
 	USER_WITH_EMAIL_NOT_FOUND: "User with this email not found.",
