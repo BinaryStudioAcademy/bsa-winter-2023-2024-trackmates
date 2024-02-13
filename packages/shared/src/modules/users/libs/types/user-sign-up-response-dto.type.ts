f19249e528d08d4aa4c8f8c695f6821d2e27416b
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2024-02-13 10:22:08 +0200

    feat: add saving token in the storage tm-12

--- packages/shared/src/modules/users/libs/types/user-sign-up-response-dto.type.ts
+++ packages/shared/src/modules/users/libs/types/user-sign-up-response-dto.type.ts
@@ -1,5 +1,8 @@
 import { UserAuthResponseDto } from "./user-auth-response-dto.type.js";
 
-type UserSignUpResponseDto = UserAuthResponseDto;
+type UserSignUpResponseDto = {
+	token: string;
+	user: UserAuthResponseDto;
+};
 
 export { type UserSignUpResponseDto };
