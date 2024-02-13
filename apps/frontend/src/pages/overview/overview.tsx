f020ca7539958de3a112b79f320397cca9ca7c4d
Matvii <wantuchmatvii@gmail.com>
2024-02-13 17:19:00 +0200

    feat: add to the header authenticated user's full name tm-25

--- apps/frontend/src/pages/overview/overview.tsx
+++ apps/frontend/src/pages/overview/overview.tsx
@@ -1,7 +1,15 @@
+import { useAppSelector } from "~/libs/hooks/hooks.js";
+
 import { WelcomeHeader } from "./components/welcome-section-header/welcom-section-header.js";
 
 const Overview: React.FC = () => {
-	return <WelcomeHeader firstName="Vlad" lastName="Zubko" />;
+	const { user } = useAppSelector((state) => state.auth);
+
+	return user ? (
+		<WelcomeHeader firstName={user.firstName} lastName={user.lastName} />
+	) : (
+		<></>
+	);
 };
 
 export { Overview };
