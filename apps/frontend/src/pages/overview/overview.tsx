b313cf93e74e66c20fd5df954f5cf9e9e9d4cc00
Matvii <wantuchmatvii@gmail.com>
2024-02-14 12:14:05 +0200

    fix: only authenticated user has access to the overview page tm-25

--- apps/frontend/src/pages/overview/overview.tsx
+++ apps/frontend/src/pages/overview/overview.tsx
@@ -1,13 +1,18 @@
 import { useAppSelector } from "~/libs/hooks/hooks.js";
 
 import { WelcomeHeader } from "./libs/components/components.js";
+import { UserAuthResponseDto } from "./libs/types/types.js";
 
 const Overview: React.FC = () => {
 	const { user } = useAppSelector((state) => state.auth);
+	const authenticatedUser = user as UserAuthResponseDto;
 
-	return user ? (
-		<WelcomeHeader firstName={user.firstName} lastName={user.lastName} />
-	) : null;
+	return (
+		<WelcomeHeader
+			firstName={authenticatedUser.firstName}
+			lastName={authenticatedUser.lastName}
+		/>
+	);
 };
 
 export { Overview };
