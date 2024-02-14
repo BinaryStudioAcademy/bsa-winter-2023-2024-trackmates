e01c9069255a8d21ed82106d5ec6156492a646a7
Maxim <toper.one111@gmail.com>
2024-02-14 08:19:42 +0200

    chore: remove duplicated logo tm-21

--- apps/frontend/src/libs/components/sidebar/sidebar.tsx
+++ apps/frontend/src/libs/components/sidebar/sidebar.tsx
@@ -1,4 +1,4 @@
-import logo from "~/assets/img/logo.png";
+import logo from "~/assets/img/svg/logo.svg";
 import { AppRoute } from "~/libs/enums/enums.js";
 import { getValidClassNames } from "~/libs/helpers/helpers.js";
 import { type ValueOf } from "~/libs/types/types.js";
