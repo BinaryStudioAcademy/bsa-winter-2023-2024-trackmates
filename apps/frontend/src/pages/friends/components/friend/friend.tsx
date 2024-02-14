85211ba91bd94daa73f5de30c043a57bf54a80ad
Maxim <toper.one111@gmail.com>
2024-02-14 12:38:56 +0200

    style: add styling for friend component tm-82

--- apps/frontend/src/pages/friends/components/friend/friend.tsx
+++ apps/frontend/src/pages/friends/components/friend/friend.tsx
@@ -11,8 +11,12 @@ const Friend: React.FC<Properties> = ({ fullName, imageUrl }: Properties) => {
 	return (
 		<div className={styles["card"]}>
 			<div>
-				<Image alt={`portrait of ${fullName}`} src={imageUrl} />
-				<p>{fullName}</p>
+				<Image
+					alt={`portrait of ${fullName}`}
+					className={styles["portrait"]}
+					src={imageUrl}
+				/>
+				<p className={styles["fullName"]}>{fullName}</p>
 			</div>
 		</div>
 	);
