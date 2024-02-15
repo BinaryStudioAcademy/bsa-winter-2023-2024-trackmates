3dc475a0d21a5f9afffbecfd6ce1bd5b16a3206f
TeaChan <valkova.tanya17@gmail.com>
2024-02-15 03:46:54 +0200

    feat: add show/hide password functionality for sign-up form tm-71

--- apps/frontend/src/pages/auth/components/sign-up-form/sign-up-form.tsx
+++ apps/frontend/src/pages/auth/components/sign-up-form/sign-up-form.tsx
@@ -1,6 +1,11 @@
-import { Button, Input, Link } from "~/libs/components/components.js";
+import {
+	Button,
+	IconButton,
+	Input,
+	Link,
+} from "~/libs/components/components.js";
 import { AppRoute } from "~/libs/enums/enums.js";
-import { useAppForm, useCallback } from "~/libs/hooks/hooks.js";
+import { useAppForm, useCallback, useState } from "~/libs/hooks/hooks.js";
 import {
 	type UserSignUpRequestDto,
 	userSignUpValidationSchema,
@@ -19,12 +24,18 @@ const SignUpForm: React.FC<Properties> = ({ onSubmit }: Properties) => {
 		validationSchema: userSignUpValidationSchema,
 	});
 
+	const [isPasswordVisible, setPasswordVisibility] = useState(false);
+
 	const handleFormSubmit = useCallback(
 		(event_: React.BaseSyntheticEvent): void => {
 			void handleSubmit(onSubmit)(event_);
 		},
 		[handleSubmit, onSubmit],
 	);
+
+	const handlePasswordVisibility = useCallback(() => {
+		setPasswordVisibility(!isPasswordVisible);
+	}, [isPasswordVisible]);
 
 	return (
 		<form className={styles["form"]} onSubmit={handleFormSubmit}>
@@ -62,14 +73,22 @@ const SignUpForm: React.FC<Properties> = ({ onSubmit }: Properties) => {
 				name="email"
 				type="text"
 			/>
-			<Input
-				color="dark"
-				control={control}
-				errors={errors}
-				label="Password"
-				name="password"
-				type="password"
-			/>
+			<div className={styles["password-input"]}>
+				<Input
+					className={styles["password"]}
+					color="dark"
+					control={control}
+					errors={errors}
+					label="Password"
+					name="password"
+					type={isPasswordVisible ? "text" : "password"}
+				/>
+				<IconButton
+					className={styles["icon"]}
+					iconName={isPasswordVisible ? "eye" : "eyeOff"}
+					onClick={handlePasswordVisibility}
+				/>
+			</div>
 			<Button color="primary" label="Create an account" type="submit" />
 		</form>
 	);
