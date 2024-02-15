0ce0dc50bc340e9ea26bb80e1817dd1131337270
Shadow <oleksandr.vpro@gmail.com>
2024-02-15 05:05:33 +0200

    feat: add IconButton component tm-59

---
+++ apps/frontend/src/libs/components/icon-button/icon-button.tsx
@@ -0,0 +1,27 @@
+import { getValidClassNames } from "~/libs/helpers/helpers.js";
+
+import { Icon } from "../icon/icon.js";
+import { type IconName } from "../icon/libs/types/types.js";
+import styles from "./styles.module.css";
+
+type Properties = {
+	className?: string | undefined;
+	iconName: IconName;
+	onClick?: React.MouseEventHandler<HTMLButtonElement>;
+};
+
+const IconButton: React.FC<Properties> = ({
+	className,
+	iconName,
+	onClick,
+}: Properties) => (
+	<button
+		className={getValidClassNames(styles["icon-button"], className)}
+		onClick={onClick}
+		type="button"
+	>
+		<Icon name={iconName} />
+	</button>
+);
+
+export { IconButton };
