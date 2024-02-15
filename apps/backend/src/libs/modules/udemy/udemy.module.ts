aaf4d949c4706f1fd5cfdc6ad7ca31fb3dcfdeaa
Kateryna Savelieva <krasilnikova.katerina@gmail.com>
2024-02-15 14:07:42 +0200

    fix: fixes after review5 - remove getAuthHeader and getOptions tm-65

--- apps/backend/src/libs/modules/udemy/udemy.module.ts
+++ apps/backend/src/libs/modules/udemy/udemy.module.ts
@@ -1,9 +1,4 @@
-import {
-	ContentType,
-	type HTTP,
-	HTTPHeader,
-	type HTTPOptions,
-} from "../http/http.js";
+import { ContentType, type HTTP, HTTPHeader } from "../http/http.js";
 import {
 	CourseDetailsField,
 	CourseField,
@@ -12,6 +7,7 @@ import {
 
 type SearchParameters = {
 	page?: number;
+	pageSize?: number;
 	search?: string;
 };
 
@@ -23,51 +19,61 @@ type Constructor = {
 };
 
 class Udemy {
-	private authHeader: string;
 	private baseUrl: string;
+	private clientId: string;
+	private clientSecret: string;
 	private http: HTTP;
 
 	public constructor({ baseUrl, clientId, clientSecret, http }: Constructor) {
-		this.authHeader = this.getAuthHeader(clientId, clientSecret);
 		this.baseUrl = baseUrl;
+		this.clientId = clientId;
+		this.clientSecret = clientSecret;
 		this.http = http;
 	}
 
-	private getAuthHeader(clientId: string, clientSecret: string): string {
-		const token = btoa(`${clientId}:${clientSecret}`);
-		return `Basic ${token}`;
-	}
-
-	private getOptions(query: Record<string, unknown>): HTTPOptions {
+	private gethHeaders(): Headers {
 		const headers = new Headers();
-		headers.append(HTTPHeader.AUTHORIZATION, this.authHeader);
+		const token = btoa(`${this.clientId}:${this.clientSecret}`);
+
+		headers.append(HTTPHeader.AUTHORIZATION, `Basic ${token}`);
 		headers.append(HTTPHeader.CONTENT_TYPE, ContentType.JSON);
 
-		return { headers, method: "GET", payload: null, query };
+		return headers;
+	}
+
+	private load(url: string, query: Record<string, unknown>): Promise<Response> {
+		return this.http.load(url, {
+			headers: this.gethHeaders(),
+			method: "GET",
+			payload: null,
+			query,
+		});
 	}
 
 	public async getCourseDetails(id: number): Promise<Response> {
-		const url = `${this.baseUrl}${id}`;
-		const options = this.getOptions({
+		const query = {
 			"fields[course]": Object.values(CourseDetailsField).join(","),
-		});
+		};
 
-		return await this.http.load(url, options);
+		return await this.load(`${this.baseUrl}${id}`, query);
 	}
 
 	public async getCourses({
 		page = UdemyDefaultSearchParameter.PAGE,
+		pageSize = UdemyDefaultSearchParameter.PAGE_SIZE,
 		search,
 	}: SearchParameters = {}): Promise<Response> {
-		const query = {
+		const query: Record<string, unknown> = {
 			"fields[course]": Object.values(CourseField).join(","),
 			page,
-			page_size: UdemyDefaultSearchParameter.PAGE_SIZE,
+			page_size: pageSize,
 		};
 
-		const options = this.getOptions(search ? { ...query, search } : query);
+		if (search) {
+			query["search"] = search;
+		}
 
-		return await this.http.load(this.baseUrl, options);
+		return await this.load(this.baseUrl, query);
 	}
 }
 
