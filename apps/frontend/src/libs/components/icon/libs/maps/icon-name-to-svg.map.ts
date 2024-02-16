c70e027e2378670ccb8f8239890b003988517b26
Tetiana Valkova <61264049+YourTeaChan@users.noreply.github.com>
2024-02-16 10:18:01 +0200

    feat: hide/show password functionality on sign-in/up page tm-71 (#89)
    * feat: add eye and eye-off icons tm-71
    
    * feat: add className prop to input component tm-71
    
    * feat: add icon-button component and its usage tm-71
    
    * feat: add show/hide password functionality for sign-in form tm-71
    
    * feat: add show/hide password functionality for sign-up form tm-71
    
    * chore: remove redundant cursor pointer tm-71
    
    * feat: remove icon-button and replace it with changed button tm-71
    
    * feat: change styles for eye-icon on sign-in/sign-up forms tm-71
    
    * fix: add type to useState tm-71
    
    * chore: rename handlePasswordVisibility to handleChangePasswordVisibility tm-71
    
    * refactor: change button structure back tm-71
    
    * refactor: add visually-hidden span and change imports tm-71
    
    * refactor: change label style tm-71

--- apps/frontend/src/libs/components/icon/libs/maps/icon-name-to-svg.map.ts
+++ apps/frontend/src/libs/components/icon/libs/maps/icon-name-to-svg.map.ts
@@ -1,3 +1,5 @@
+import EyeIcon from "~/assets/img/svg/eye.svg?react";
+import EyeOffIcon from "~/assets/img/svg/eye-off.svg?react";
 import HomeIcon from "~/assets/img/svg/home.svg?react";
 import PlusIcon from "~/assets/img/svg/plus.svg?react";
 import { type IconName } from "~/libs/types/types.js";
@@ -6,6 +8,8 @@ const iconNameToSvg: Record<
 	IconName,
 	React.FC<React.SVGProps<SVGSVGElement>>
 > = {
+	eye: EyeIcon,
+	eyeOff: EyeOffIcon,
 	home: HomeIcon,
 	plus: PlusIcon,
 };
