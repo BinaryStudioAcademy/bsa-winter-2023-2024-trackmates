d1e4177af7e856507abb2e8e4a05fc86c252a1e2
Shadow <oleksandr.vpro@gmail.com>
2024-02-16 19:05:18 +0200

    refactor: update checkIfShowLoader helper imports tm-95

--- apps/frontend/src/libs/helpers/check-if-show-loader/check-if-show-loader.helper.ts
+++ apps/frontend/src/libs/helpers/check-if-show-loader/check-if-show-loader.helper.ts
@@ -1,6 +1,5 @@
-import { ValueOf } from "shared";
-
 import { DataStatus } from "~/libs/enums/enums.js";
+import { ValueOf } from "~/libs/types/value-of.type.js";
 
 const checkIfShowLoader = (dataStatus: ValueOf<typeof DataStatus>) => {
 	return dataStatus === DataStatus.IDLE || dataStatus === DataStatus.PENDING;
