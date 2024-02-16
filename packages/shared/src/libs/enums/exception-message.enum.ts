0a4a22d6a309d870f681bc989e4282bc289662f3
Shadow <oleksandr.vpro@gmail.com>
2024-02-16 14:35:45 +0200

    feat: add UNAUTHORIZED and TOKEN_EXPIRED to ExceptionMessage enum tm-95

--- packages/shared/src/libs/enums/exception-message.enum.ts
+++ packages/shared/src/libs/enums/exception-message.enum.ts
@@ -2,6 +2,8 @@ const ExceptionMessage = {
 	EMAIL_ALREADY_EXISTS: "Email is already taken.",
 	INCORRECT_CREDENTIALS: "Incorrect credentials.",
 	INVALID_TOKEN: "Token is invalid.",
+	TOKEN_EXPIRED: "Token expired.",
+	UNAUTHORIZED: "Unauthorized.",
 	UNKNOWN_ERROR: "Unknown error occurred.",
 	USER_NOT_FOUND: "User not found.",
 	USER_WITH_EMAIL_NOT_FOUND: "User with this email not found.",
