6dd6422ef124d5d610c5e786e94ba52ccfecaf8f
Vfivew <hitori555111555@yahoo.com>
2024-02-17 17:17:36 +0200

    fix: fixed lint error tm-39

--- packages/shared/src/modules/friends/libs/types/type.ts
+++ packages/shared/src/modules/friends/libs/types/type.ts
@@ -1,5 +1,5 @@
 export { type FriendAcceptResponseDto } from "./friend-accept-response-dto.type.js";
 export { type FriendAddNewRequestDto } from "./friend-add-new-request-dto.type.js";
+export { type FriendDto } from "./friend-dto.type.js";
 export { type FriendReplyRequestDto } from "./friend-reply-request-dto.type.js";
 export { type FriendResponseDto } from "./frined-response-dto.type.js";
-export { type FriendDto } from "./friend-dto.type.js"
--- packages/shared/src/modules/friends/libs/types/types.ts
+++
@@ -1,1 +0,0 @@
-export { type FriendDto } from "./friend-dto.type.js";
