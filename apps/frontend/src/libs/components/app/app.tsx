ef31eaecbde56e6ade0c5409c5986fdd7a3c4dbc
Ann Maksai <annamaksai99@gmail.com>
2024-02-17 18:26:45 +0200

    fix: the extra space at the top of the pages tm-104 (#109)
    * fix: remove unnecessary exports tm-104
    
    * fix: fix svg difference tm-104
    
    * fix: remove mock data tm-104
    
    * fix: gitignore dev-dist tm-104
    
    * fix: types for backend tm-104
    
    * fix: file name tm-104
    
    * fix: remove users controller tm-104

--- apps/frontend/src/libs/components/app/app.tsx
+++ apps/frontend/src/libs/components/app/app.tsx
@@ -1,33 +1,21 @@
 import { Loader, RouterOutlet } from "~/libs/components/components.js";
-import { AppRoute, DataStatus } from "~/libs/enums/enums.js";
+import { DataStatus } from "~/libs/enums/enums.js";
 import {
 	useAppDispatch,
 	useAppSelector,
 	useEffect,
-	useLocation,
 } from "~/libs/hooks/hooks.js";
 import { actions as authActions } from "~/modules/auth/auth.js";
-import { actions as userActions } from "~/modules/users/users.js";
 
 const App: React.FC = () => {
-	const { pathname } = useLocation();
 	const dispatch = useAppDispatch();
-	const { authDataStatus, users, usersDataStatus } = useAppSelector(
-		({ auth, users }) => ({
-			authDataStatus: auth.dataStatus,
-			users: users.users,
-			usersDataStatus: users.dataStatus,
-		}),
-	);
-
-	const isRoot = pathname === AppRoute.ROOT;
+	const { authDataStatus } = useAppSelector(({ auth }) => ({
+		authDataStatus: auth.dataStatus,
+	}));
 
 	useEffect(() => {
-		if (isRoot) {
-			void dispatch(userActions.loadAll());
-		}
 		void dispatch(authActions.getAuthenticatedUser());
-	}, [isRoot, dispatch]);
+	}, [dispatch]);
 
 	if (
 		authDataStatus === DataStatus.IDLE ||
@@ -38,15 +26,7 @@ const App: React.FC = () => {
 
 	return (
 		<>
-			<ul className="visually-hidden">
-				{users.map((user) => (
-					<li key={user.id}>{user.email}</li>
-				))}
-			</ul>
 			<div>
-				{usersDataStatus === DataStatus.PENDING && (
-					<Loader color="orange" size="large" />
-				)}
 				<RouterOutlet />
 			</div>
 		</>
