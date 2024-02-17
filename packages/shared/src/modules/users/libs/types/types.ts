fef3bc73206d6389668e8dc493558f06f6cadd2f
Anna Maksai <annamaksai99@gmail.com>
2024-02-17 12:54:08 +0200

    fix: file name tm-104

--- packages/shared/src/modules/users/libs/types/types.ts
+++ packages/shared/src/modules/users/libs/types/types.ts
@@ -1,5 +1,5 @@
 export { type UserAuthResponseDto } from "./user-auth-response-dto.type.js";
-export { type UserGetAllResponseDto } from "./user-get-all-response-dto.js";
+export { type UserGetAllResponseDto } from "./user-get-all-response-dto.type.js";
 export { type UserSignInRequestDto } from "./user-sign-in-request-dto.type.js";
 export { type UserSignInResponseDto } from "./user-sign-in-response-dto.type.js";
 export { type UserSignUpRequestDto } from "./user-sign-up-request-dto.type.js";
