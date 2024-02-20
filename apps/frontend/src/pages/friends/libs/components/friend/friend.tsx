9b32f36ad42fdba19ed9ab207b5f1e82ef4211a2
Maxim <toper.one111@gmail.com>
2024-02-20 20:24:42 +0200

    chore(frontend): add friend image to public tm-94

--- apps/frontend/src/pages/friends/libs/components/friend/friend.tsx
+++ apps/frontend/src/pages/friends/libs/components/friend/friend.tsx
@@ -1,3 +1,4 @@
+import friendImage from "~/assets/img/friend.jpeg";
 import { Button, Image } from "~/libs/components/components.js";
 import {
 	useAppDispatch,
@@ -37,16 +38,13 @@ const Friend: React.FC<Properties> = ({ friend }: Properties) => {
 	}, [dispatch, friend.id]);
 	const fullName = `${friend.firstName} ${friend.lastName}`;
 
-	const imageUrl =
-		"https://s3-alpha-sig.figma.com/img/e5a8/4d2a/8468b40071144244537e7aa36b4d9354?Expires=1708905600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=YabQdizET7TaHqgYdN~Kai8uMZBSMzSZ0rZL2goz3TgB0QE14X~jzsIvcN5yRmYT~dT5q3Xo33ThriO0QmovboskT6Um5OagC-Zyj-zu40pczDLI7L7-sw15AXjfIWA1G5nPCPPQ30lmLG999MM-LY9v~a1BnhmwDJ1N9r7bPaBOA8PEEmJ4RDSCfnhJC9sDdHUG8-VJGh04amY7NCrKHJjhTYW5iEIlkFiN5pkl7HhTCh-IWckvqZAeB-qTcfhb6AW60gf0IqzzNIO-KeLz1LdpCvSe0Ynty5W4qkLJFwtqCA2jBPkTRfk~iFgspQtBE7Xy3u~0j4EK8dKxx9~RbQ__";
-
 	return (
 		<article className={styles["card"]}>
 			<div className={styles["card-content"]}>
 				<Image
 					alt={`portrait of ${fullName}`}
 					className={styles["portrait"]}
-					src={imageUrl}
+					src={friendImage}
 				/>
 				<p className={styles["fullName"]}>{fullName}</p>
 			</div>
