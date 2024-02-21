12cb8ec39bf7db2c6d2e3776a289741c5c6d030a
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2024-02-21 12:37:41 +0200

    fix(frontend): update slices tm-42

--- apps/frontend/src/modules/users/slices/users.slice.ts
+++ apps/frontend/src/modules/users/slices/users.slice.ts
@@ -2,43 +2,19 @@ import { createSlice } from "@reduxjs/toolkit";
 
 import { DataStatus } from "~/libs/enums/enums.js";
 import { type ValueOf } from "~/libs/types/types.js";
-import { type UserAuthResponseDto } from "~/modules/auth/auth.js";
-
-import { updateProfile, updateUserAvatar } from "./actions.js";
+import { type UserAuthResponseDto } from "~/modules/users/users.js";
 
 type State = {
 	dataStatus: ValueOf<typeof DataStatus>;
-	user: UserAuthResponseDto | null;
+	users: UserAuthResponseDto[];
 };
 
 const initialState: State = {
 	dataStatus: DataStatus.IDLE,
-	user: null,
+	users: [],
 };
 
 const { actions, name, reducer } = createSlice({
-	extraReducers(builder) {
-		builder.addCase(updateProfile.pending, (state) => {
-			state.dataStatus = DataStatus.PENDING;
-		});
-		builder.addCase(updateProfile.fulfilled, (state, action) => {
-			state.dataStatus = DataStatus.FULFILLED;
-			state.user = { ...state.user, ...action.payload };
-		});
-		builder.addCase(updateProfile.rejected, (state) => {
-			state.dataStatus = DataStatus.REJECTED;
-		});
-		builder.addCase(updateUserAvatar.pending, (state) => {
-			state.dataStatus = DataStatus.PENDING;
-		});
-		builder.addCase(updateUserAvatar.fulfilled, (state, action) => {
-			state.dataStatus = DataStatus.FULFILLED;
-			state.user = action.payload;
-		});
-		builder.addCase(updateUserAvatar.rejected, (state) => {
-			state.dataStatus = DataStatus.REJECTED;
-		});
-	},
 	initialState,
 	name: "users",
 	reducers: {},
