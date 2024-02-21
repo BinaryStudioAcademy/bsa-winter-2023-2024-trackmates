f298f06f39179b92a88c79de7943fc5019ad9895
Matvii <wantuchmatvii@gmail.com>
2024-02-21 18:29:22 +0200

    refactor(backend): move aws s3 integration logic into the file service tm-41

--- apps/backend/src/modules/files/file.service.ts
+++ apps/backend/src/modules/files/file.service.ts
@@ -1,3 +1,6 @@
+import { S3Client } from "@aws-sdk/client-s3";
+import { Upload } from "@aws-sdk/lib-storage";
+
 import { ExceptionMessage } from "~/libs/enums/enums.js";
 import { HTTPCode, HTTPError } from "~/libs/modules/http/http.js";
 import { type Service } from "~/libs/types/service.type.js";
@@ -5,30 +8,70 @@ import { type FileRepository } from "~/modules/files/file.repository.js";
 import { type UserService } from "~/modules/users/users.js";
 
 import { FileEntity } from "./file.entity.js";
-import { type File } from "./file.module.js";
 import {
 	type FileUploadResponseDto,
 	type UploadedFile,
 } from "./libs/types/types.js";
 
+type Constructor = {
+	credentials: {
+		accessKeyId: string;
+		bucket: string;
+		region: string;
+		secretAccessKey: string;
+	};
+	fileRepository: FileRepository;
+	userService: UserService;
+};
+
 class FileService implements Service {
-	private file: File;
+	private accessKeyId: string;
+	private bucket: string;
 	private fileRepository: FileRepository;
+	private region: string;
+	private secretAccessKey: string;
 	private userService: UserService;
 
-	constructor(
-		file: File,
-		fileRepository: FileRepository,
-		userService: UserService,
-	) {
-		this.file = file;
+	constructor({ credentials, fileRepository, userService }: Constructor) {
+		this.accessKeyId = credentials.accessKeyId;
+		this.bucket = credentials.bucket;
 		this.fileRepository = fileRepository;
+		this.region = credentials.region;
+		this.secretAccessKey = credentials.secretAccessKey;
 		this.userService = userService;
+	}
+
+	private async upload(file: Buffer, fileName: string): Promise<string> {
+		return await new Upload({
+			client: new S3Client({
+				credentials: {
+					accessKeyId: this.accessKeyId,
+					secretAccessKey: this.secretAccessKey,
+				},
+				region: this.region,
+			}),
+			params: {
+				ACL: "public-read",
+				Body: file,
+				Bucket: this.bucket,
+				Key: `${Date.now().toString()}-${fileName}`,
+			},
+		})
+			.done()
+			.then((result) => {
+				if (result.Location) {
+					return result.Location;
+				}
+				throw new HTTPError({
+					message: ExceptionMessage.UNKNOWN_ERROR,
+					status: HTTPCode.INTERNAL_SERVER_ERROR,
+				});
+			});
 	}
 
 	async create(uploadedFile: UploadedFile): Promise<FileUploadResponseDto> {
 		const { buffer, contentType, fileName } = uploadedFile;
-		const url = await this.file.upload(buffer, fileName);
+		const url = await this.upload(buffer, fileName);
 		const file = await this.fileRepository.create(
 			FileEntity.initializeNew({ contentType, url }),
 		);
@@ -80,7 +123,7 @@ class FileService implements Service {
 		uploadedFile: UploadedFile,
 	): Promise<FileUploadResponseDto> {
 		const { buffer, contentType, fileName } = uploadedFile;
-		const url = await this.file.upload(buffer, fileName);
+		const url = await this.upload(buffer, fileName);
 		const file = await this.fileRepository.create(
 			FileEntity.initializeNew({ contentType, url }),
 		);
--- apps/backend/src/modules/files/files.ts
+++ apps/backend/src/modules/files/files.ts
@@ -4,18 +4,20 @@ import { userService } from "~/modules/users/users.js";
 
 import { FileController } from "./file.controller.js";
 import { FileModel } from "./file.model.js";
-import { File } from "./file.module.js";
 import { FileRepository } from "./file.repository.js";
 import { FileService } from "./file.service.js";
 
-const fileModule = new File({
-	accessKeyId: config.ENV.AWS_S3.AWS_ACCESS_KEY_ID,
-	bucket: config.ENV.AWS_S3.S3_BUCKET,
-	region: config.ENV.AWS_S3.S3_REGION,
-	secretAccessKey: config.ENV.AWS_S3.AWS_SECRET_ACCESS_KEY,
-});
 const fileRepository = new FileRepository(FileModel);
-const fileService = new FileService(fileModule, fileRepository, userService);
+const fileService = new FileService({
+	credentials: {
+		accessKeyId: config.ENV.AWS_S3.AWS_ACCESS_KEY_ID,
+		bucket: config.ENV.AWS_S3.S3_BUCKET,
+		region: config.ENV.AWS_S3.S3_REGION,
+		secretAccessKey: config.ENV.AWS_S3.AWS_SECRET_ACCESS_KEY,
+	},
+	fileRepository,
+	userService,
+});
 const fileController = new FileController(logger, fileService);
 
 export { fileController };
