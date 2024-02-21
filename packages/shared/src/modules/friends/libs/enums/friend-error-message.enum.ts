b7e51c685d12d5a6f09de47c82e275872856f186
Vfivew <hitori555111555@yahoo.com>
2024-02-21 17:06:33 +0200

    fix(shared): fix error enums tm-39

--- packages/shared/src/modules/friends/libs/enums/friend-error-message.enum.ts
+++ packages/shared/src/modules/friends/libs/enums/friend-error-message.enum.ts
@@ -1,10 +1,9 @@
 const FriendErrorMessage = {
-	FRIEND_REQUEST_ERROR: "You are not subscribed to this user.",
-	FRIEND_REQUEST_EXIST: "You already follow this user.",
-	FRIEND_UNFOLLOW_ERROR: "You are not following this user.",
-	FRIEND_UPDATE_ERROR:
-		"Friend update error: Unable to update the subscription.",
-	SEND_REQUEST_TO_YOURSELF: "You follow yourself.",
+	FRIEND_REQUEST_ERROR: "You are not subscribed to this user",
+	FRIEND_REQUEST_EXIST: "You already follow this user",
+	FRIEND_UNFOLLOW_ERROR: "You are not following this user",
+	FRIEND_UPDATE_ERROR: "Friend update error: Unable to update the subscription",
+	SEND_REQUEST_TO_YOURSELF: "You follow yourself",
 } as const;
 
 export { FriendErrorMessage };
