b2882aaf5fddcdbbfd5ff9f97d646cc1035016f8
Matvii <wantuchmatvii@gmail.com>
2024-02-22 13:20:15 +0200

    fix(backend): change repository update method arguments and return type tm-41

--- apps/backend/src/libs/types/repository.type.ts
+++ apps/backend/src/libs/types/repository.type.ts
@@ -3,7 +3,7 @@ type Repository<T = unknown> = {
 	delete(id: number): Promise<boolean>;
 	find(id: number): Promise<T | null>;
 	findAll(): Promise<T[]>;
-	update(id: number, payload: Partial<T>): Promise<T>;
+	update(id: number, payload: unknown): Promise<T | null>;
 };
 
 export { type Repository };
