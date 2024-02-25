3ae465a147ef5bcbe3f8cfcb7703ede02ee4ca48
TeaChan <valkova.tanya17@gmail.com>
2024-02-25 12:41:22 +0200

    feat(backend): add optional temperature prop tm-43

--- apps/backend/src/libs/modules/open-ai/open-ai.module.ts
+++ apps/backend/src/libs/modules/open-ai/open-ai.module.ts
@@ -2,6 +2,7 @@ import { OpenAI as LibraryOpenAI } from "openai";
 
 import { HTTPCode } from "~/libs/enums/enums.js";
 
+import { DEFAULT_TEMPERATURE_VALUE } from "./libs/constants/constants.js";
 import { OpenAIErrorMessage } from "./libs/enums/enums.js";
 import { OpenAIError } from "./libs/exceptions/exceptions.js";
 
@@ -19,12 +20,15 @@ class OpenAI {
 		this.openAI = new LibraryOpenAI({ apiKey });
 	}
 
-	public async call(request: string): Promise<string> {
+	public async call(
+		request: string,
+		temperature: number = DEFAULT_TEMPERATURE_VALUE,
+	): Promise<string> {
 		const response = await this.openAI.chat.completions.create({
 			messages: [{ content: request, role: "user" }],
 			model: this.model,
 			response_format: { type: "text" },
-			temperature: 0,
+			temperature,
 		});
 
 		const [choice] = response.choices;
