53ade6d56ac64f7ddfdf13b28b6b618c3ee6c85b
Vfivew <hitori555111555@yahoo.com>
2024-02-26 17:23:32 +0200

    fix(frontend): delete helper tm-174

--- apps/frontend/src/libs/helpers/get-first-path-segment/get-first-path-segment.helper.ts
+++
@@ -1,7 +0,0 @@
-const getFirstPathSegment = (path: string): string => {
-	const match = /^\/(?<segment>[^/]+)/.exec(path);
-
-	return match?.groups?.["segment"] ? `/${match.groups["segment"]}` : "/";
-};
-
-export { getFirstPathSegment };
--- apps/frontend/src/libs/helpers/helpers.ts
+++ apps/frontend/src/libs/helpers/helpers.ts
@@ -1,3 +1,2 @@
-export { getFirstPathSegment } from "./get-first-path-segment/get-first-path-segment.helper.js";
 export { getValidClassNames } from "./get-valid-class-names/get-valid-class-names.helper.js";
 export { configureString } from "@trackmates/shared";
