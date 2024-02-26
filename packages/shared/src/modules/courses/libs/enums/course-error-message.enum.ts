c72eb475c74e92a903429e0c954b70c4735fe5b9
Kateryna Savelieva <krasilnikova.katerina@gmail.com>
2024-02-26 17:37:24 +0200

    fix(shared): fix error messages tm-27

--- packages/shared/src/modules/courses/libs/enums/course-error-message.enum.ts
+++ packages/shared/src/modules/courses/libs/enums/course-error-message.enum.ts
@@ -1,10 +1,10 @@
 const CourseErrorMessage = {
 	COURSE_IS_ALREADY_ADDED_FOR_USER: "Course was already added for user.",
-	COURSE_IS_ALREADY_EXISTS: "Course was already added for user.",
-	NOT_FOUND_API_FOR_VENDOR: "Not found api for vendor.",
-	NOT_FOUND_COURSE: "Not found course.",
-	NOT_FOUND_USER: "Not found user.",
-	NOT_FOUND_VENDOR: "Not found vendor.",
+	COURSE_IS_ALREADY_EXISTS: "Course is already exists.",
+	NOT_FOUND_API_FOR_VENDOR: "Api for vendor not found.",
+	NOT_FOUND_COURSE: "Course not found.",
+	NOT_FOUND_USER: "User not found.",
+	NOT_FOUND_VENDOR: "Vendor not found.",
 } as const;
 
 export { CourseErrorMessage };
