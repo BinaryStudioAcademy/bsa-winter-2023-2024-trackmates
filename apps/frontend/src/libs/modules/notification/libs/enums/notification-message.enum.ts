ca46e10103bef4ad25a7b754abd9fddc58485644
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2024-02-27 20:14:35 +0200

    chore(frontend): change notification message of profile tm-42

--- apps/frontend/src/libs/modules/notification/libs/enums/notification-message.enum.ts
+++ apps/frontend/src/libs/modules/notification/libs/enums/notification-message.enum.ts
@@ -2,8 +2,8 @@ const NotificationMessage = {
 	COURSE_ADDED: "Course successfully added to your account.",
 	FRIEND_FOLLOW_SUCCESS: "Followed successfully.",
 	FRIEND_UNFOLLOW_SUCCESS: "Unfollowed successfully.",
-	PROFILE_CHANGES_NOT_SAVED: "Changes are not saved",
-	PROFILE_CHANGES_SAVED: "Changes are saved",
+	PROFILE_CHANGES_NOT_SAVED: "Please, update your changes before leaving.",
+	PROFILE_CHANGES_SAVED: "Changes are saved.",
 } as const;
 
 export { NotificationMessage };
