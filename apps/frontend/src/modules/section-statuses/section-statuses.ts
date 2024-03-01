d33eaf2afac2757d04c0c8538189d3d1690fd308
Verhusha Oleg <olegverhusha@gmail.com>
2024-03-01 13:50:41 +0200

    feat(frontend): add section-status module tm-161

---
+++ apps/frontend/src/modules/section-statuses/section-statuses.ts
@@ -0,0 +1,15 @@
+import { config } from "~/libs/modules/config/config.js";
+import { http } from "~/libs/modules/http/http.js";
+import { storage } from "~/libs/modules/storage/storage.js";
+
+import { SectionStatusApi } from "./section-statuses-api.js";
+
+const sectionStatusApi = new SectionStatusApi({
+	baseUrl: config.ENV.API.ORIGIN_URL,
+	http,
+	storage,
+});
+
+export { sectionStatusApi };
+export { SectionStatus } from "./libs/enums/enums.js";
+export { actions, reducer } from "./slices/section-statuses.js";
