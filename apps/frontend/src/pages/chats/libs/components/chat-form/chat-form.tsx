fa01e712149811184e8f07ec51521324bcd86a83
Ann Maksai <annamaksai99@gmail.com>
2024-03-01 12:24:29 +0200

    feat(frontend/shared): add chat functionality tm-112 (#222)
    * feat: add chat_messages table tm-111
    
    * feat: add ChatMessage model, types and enums tm-111
    
    * feat: add Chat and ChatMessage entities tm-111
    
    * feat: add ChatMessage repository tm-111
    
    * feat: add ChatMessage service tm-111
    
    * feat: add ChatMessage controller tm-111
    
    * feat: add swagger docs tm-111
    
    * feat: configure params validation tm-111
    
    * feat: add ChatMessage validation tm-111
    
    * chore(backend/shared): rename chat to chat-message package tm-111
    
    * chore(backend): remove destructuration from ChatMessage controller params tm-111
    
    * refactor(backend): add EMPTY_CHAT_LENGTH constant tm-111
    
    * feat(frontend/shared): add chats page, components, types and actions tm-112
    
    * fix(frontend): fix some styles, mock data and endpoint tm-112
    
    * refactor(backend, shared): rename chatId parameter tm-111
    
    * chore(shared): update ChatValidationMessage enum tm-111
    
    * chore(shared/backend): update ChatGetAllResponseDto and MessageGetAllResponseDto tm-111
    
    * chore(backend/shared): update APIPath enum tm-111
    
    * refactor(backend): add TableName enum tm-111
    
    * chore(backend): update variable name tm-111
    
    * feat(backend): change Repository type and implement all methods in ChatMessageRepository tm-111
    
    * feat(frontend): add app action navigate, change createChat action tm-112
    
    * refactor(backend): remove redundant relation and update query tm-111
    
    * fix(frontend/shared): validation-schema name tm-112
    
    * chore(backend/shared): delete old chat-message module tm-111
    
    * feat(backend): add new migrations tm-111
    
    * feat(shared): add chats and chat-messages types and enums tm-111
    
    * feat(backend): add chats and chat-messages modules tm-111
    
    * feat(backend/shared): update findByMembersIds method tm-111
    
    * style(backend): update files tm-111
    
    * feat(backend/shared): add update message method tm-111
    
    * feat(backend): add findAll messages method tm-111
    
    * feat(frontend): add back button, fix styles, remove autofill tm-112
    
    * feat(backend): add delete message method tm-111
    
    * feat(backend): add find message method tm-111
    
    * feat(backend): add delete chat method tm-111
    
    * feat(backend): add update chat method tm-111
    
    * fix(frontend): fix code style tm-112
    
    * feat(backend/shared): add ChatError exception tm-111
    
    * chore(backend/shared): update imports tm-111
    
    * feat(backend/shared): add chats and chat-messages validation tm-111
    
    * feat(backend/shared): add mutual following check tm-111
    
    * feat(backend): add swagger docs to Chats and ChatMessages modules tm-111
    
    * chore: update db schema in readme tm-111
    
    * fix(shared): change names tm-112
    
    * refactor(backend): update service type tm-111
    
    * chore(backend): rename getIsMutualFollowersByIds to checkIsMutualFollowersByIds method tm-111
    
    * chore(backend): update checkIsMutualFollowersByIds method tm-111
    
    * style(backend): update findAll method tm-111
    
    * feat(backend/shared): add url parameters validation tm-111
    
    * chore(backend/shared): rename ChatSingleItemResponseDto to ChatItemResponseDto tm-111
    
    * fix(frontend): fix empty chats, remove unnecessary helpers, move app slice from modules tm-112
    
    * fix(frontend): remove unnecessary condition tm-112
    
    * fix(frontend/shared): fix names and styles tm-112
    
    * fix(frontend): simplified condition tm-112
    
    * fix(frontend/shared): add date-fns, refactor dateFormat tm-112
    
    * fix: merge main tm-112
    
    * fix(frontend): changing the message icon according to the QA team's advice tm-112
    
    * fix(frontend): removed unnecessary selectors, fixed imports tm-112
    
    * fix(shared): refactor format date tm-112
    
    ---------
    
    Co-authored-by: Shadow <oleksandr.vpro@gmail.com>

---
+++ apps/frontend/src/pages/chats/libs/components/chat-form/chat-form.tsx
@@ -0,0 +1,53 @@
+import { Button, Input } from "~/libs/components/components.js";
+import { useAppForm, useCallback } from "~/libs/hooks/hooks.js";
+import { chatMessageValidationSchema } from "~/modules/chat-messages/chat-messages.js";
+
+import { DEFAULT_MESSAGE_PAYLOAD } from "../../constants/constants.js";
+import styles from "./styles.module.css";
+
+type Properties = {
+	onSubmit: (payload: typeof DEFAULT_MESSAGE_PAYLOAD) => Promise<void>;
+};
+
+const ChatForm: React.FC<Properties> = ({ onSubmit }: Properties) => {
+	const { control, errors, handleSubmit, reset } = useAppForm<
+		typeof DEFAULT_MESSAGE_PAYLOAD
+	>({
+		defaultValues: DEFAULT_MESSAGE_PAYLOAD,
+		validationSchema: chatMessageValidationSchema,
+	});
+
+	const handleFormSubmit = useCallback(
+		(event_: React.BaseSyntheticEvent): void => {
+			void handleSubmit(onSubmit)(event_);
+			reset();
+		},
+		[handleSubmit, onSubmit, reset],
+	);
+
+	return (
+		<form
+			autoComplete="off"
+			className={styles["form"]}
+			onSubmit={handleFormSubmit}
+		>
+			<Input
+				className={styles["input"]}
+				control={control}
+				errors={errors}
+				hasVisuallyHiddenLabel
+				label="Send a message"
+				name="message"
+			/>
+			<Button
+				className={styles["icon"]}
+				hasVisuallyHiddenLabel
+				iconName="message"
+				label="Send a message"
+				type="submit"
+			/>
+		</form>
+	);
+};
+
+export { ChatForm };
---
+++ packages/shared/src/libs/helpers/date/date.ts
@@ -0,0 +1,2 @@
+export { getDifferenceInHours } from "./get-difference-in-hours.helper.js";
+export { getFormattedDate } from "./get-formatted-date.helper.js";
