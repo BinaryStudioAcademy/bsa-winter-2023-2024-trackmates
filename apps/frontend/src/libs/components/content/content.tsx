600ccbcc5f8966332213ae9afd61be7fd10452f0
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2024-03-01 14:07:40 +0200

    fix(frontend): rename constants tm-28

--- apps/frontend/src/libs/components/content/content.tsx
+++ apps/frontend/src/libs/components/content/content.tsx
@@ -1,6 +1,6 @@
 import sanitizeHtml from "sanitize-html";
 
-import { MAXIMUM_LENGTH, START_INDEX } from "./libs/constants.js";
+import { MAXIMUM_CONTENT_LENGTH, START_INDEX } from "./libs/constants.js";
 
 type Properties = {
 	string: string;
@@ -9,11 +9,12 @@ type Properties = {
 const Content: React.FC<Properties> = ({ string }: Properties) => {
 	const sanitizedString = sanitizeHtml(string);
 
-	if (string.length <= MAXIMUM_LENGTH) {
+	if (string.length <= MAXIMUM_CONTENT_LENGTH) {
 		return <div dangerouslySetInnerHTML={{ __html: sanitizedString || "" }} />;
 	}
 
-	const truncatedContent = string.slice(START_INDEX, MAXIMUM_LENGTH) + "...";
+	const truncatedContent =
+		string.slice(START_INDEX, MAXIMUM_CONTENT_LENGTH) + "...";
 
 	return <div dangerouslySetInnerHTML={{ __html: truncatedContent }} />;
 };
