d645d588f56d38a21dab2b79f186ab68a133a182
Matvii <wantuchmatvii@gmail.com>
2024-03-01 23:37:42 +0200

    fix(frontend): remove star component and add stars directly into image tm-198

--- apps/frontend/src/pages/notifications/notifications.tsx
+++ apps/frontend/src/pages/notifications/notifications.tsx
@@ -8,7 +8,6 @@ import {
 import { actions } from "~/modules/user-notifications/user-notifications.js";
 
 import { NotificationList } from "./libs/components/notification-list/notification-list.js";
-import { Star } from "./libs/components/star/star.js";
 import styles from "./styles.module.css";
 
 const Notifications: React.FC = () => {
@@ -29,14 +28,6 @@ const Notifications: React.FC = () => {
 				<NotificationList notifications={notifications} />
 			</div>
 			<div className={styles["background"]}>
-				<Star height="13px" left="25%" top="10%" />
-				<Star height="15px" left="70%" top="15%" />
-				<Star height="22px" left="45%" top="18%" />
-				<Star height="20px" left="15%" top="45%" />
-				<Star height="18px" left="80%" top="50%" />
-				<Star height="18px" left="25%" top="85%" />
-				<Star height="15px" left="50%" top="75%" />
-				<Star height="10px" left="75%" top="80%" />
 				<Image
 					alt="notification character"
 					className={styles["notification-character"]}
