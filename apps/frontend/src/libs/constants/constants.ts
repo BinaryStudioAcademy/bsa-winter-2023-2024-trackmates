fc6a72c588ba3025b0bd9215ae901f358475a4de
Vlad Manukian <141722848+Vfivew@users.noreply.github.com>
2024-03-02 20:40:34 +0200

    fix(frontend): fix default pictures on the friend page tm-266 (#279)
    * fix(backend/frontend): fix default pictures on the friends page tm-266
    
    * fix(frontend): delete old image tm-266
    
    * fix(frontend): fix changelog tm-266
    
    * fix(frontend): change def photo and width tm-266
    
    * fix(frontend): change default photo every where tm-266
    
    * fix(frontend): fix avatar position tm-266

--- apps/frontend/src/libs/constants/constants.ts
+++ apps/frontend/src/libs/constants/constants.ts
@@ -1,4 +1,3 @@
 export { MENU_ITEMS } from "./menu-items.constants.js";
 export { PAGES_WITH_SEARCH_BAR } from "./pages-with-search-bar.constant.js";
-export { DEFAULT_USER_AVATAR } from "./user.constants.js";
 export { EMPTY_ARRAY_LENGTH } from "@trackmates/shared";
