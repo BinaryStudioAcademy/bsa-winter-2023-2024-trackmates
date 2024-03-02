29538561641471e04f3c30d26770d22c1de8142a
Oleksandr Tkachuk <61262913+ShadowOfTheSpace@users.noreply.github.com>
2024-03-02 18:29:57 +0200

    feat(frontend): search field mobile responsiveness tm-227 (#263)
    * feat(frontend): add PAGES_WITHOUT_SEARCH_BAR constant tm-227
    
    * feat(frontend): update search bar and its styles tm-227
    
    * feat(frontend): update header and its styles tm-227
    
    * chore(frontend): update search bar props names tm-227
    
    * refactor(frontend): update media queries usage tm-227
    
    * feat(frontend): add isPathMatchingPattern helper tm-227
    
    * refactor(frontend): add usage of isPathMatchingPattern helper tm-227
    
    * chore(frontend): update helper name tm-227

--- apps/frontend/src/libs/constants/constants.ts
+++ apps/frontend/src/libs/constants/constants.ts
@@ -1,3 +1,4 @@
 export { MENU_ITEMS } from "./menu-items.constants.js";
+export { PAGES_WITH_SEARCH_BAR } from "./pages-with-search-bar.constant.js";
 export { DEFAULT_USER_AVATAR } from "./user.constants.js";
 export { EMPTY_ARRAY_LENGTH } from "@trackmates/shared";
