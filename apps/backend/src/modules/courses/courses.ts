22c52d8b5b038387b235f5b5ee9b0aa87409bc22
Maxim <toper.one111@gmail.com>
2024-03-03 12:52:16 +0200

    chore(backend): remove unnecessary change tm-206

--- apps/backend/src/modules/courses/courses.ts
+++ apps/backend/src/modules/courses/courses.ts
@@ -22,7 +22,7 @@ const courseService = new CourseService({
 });
 const courseController = new CourseController(logger, courseService);
 
+export { courseController, courseService };
 export { CourseModel } from "./course.model.js";
 export { type CourseService } from "./course.service.js";
 export { type CourseDto } from "./libs/types/types.js";
-export { courseController, courseService };
