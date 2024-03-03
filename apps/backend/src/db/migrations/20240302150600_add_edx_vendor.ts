a1190025ba7bf4131cd4f0fca33590add1d28884
Anna Maksai <annamaksai99@gmail.com>
2024-03-03 19:43:38 +0200

    fix(backend): migration tm-211

--- apps/backend/src/db/migrations/20240302150600_add_edx_vendor.ts
+++ apps/backend/src/db/migrations/20240302150600_add_edx_vendor.ts
@@ -1,16 +1,16 @@
 import { type Knex } from "knex";
 
-const TableName = "vendors";
+const TABLE_NAME = "vendors";
 
 const KEY_COLUMN = "key";
-const row = { key: "edx", name: "edX", url: "https://www.edx.org" };
+const Row = { key: "edx", name: "edX", url: "https://www.edx.org" } as const;
 
 async function up(knex: Knex): Promise<void> {
-	await knex(TableName).insert([row]);
+	await knex(TABLE_NAME).insert([Row]);
 }
 
 async function down(knex: Knex): Promise<void> {
-	await knex(TableName).where(KEY_COLUMN, row[KEY_COLUMN]).del();
+	await knex(TABLE_NAME).where(KEY_COLUMN, Row[KEY_COLUMN]).del();
 }
 
 export { down, up };
