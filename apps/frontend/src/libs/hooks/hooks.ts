957ebf70faa09672204f34610ec3ae94831d3db3
Oleg Verhusha <91093431+tarmiel@users.noreply.github.com>
2024-03-05 17:42:07 +0200

    feat(frontend/backend/shared): add pagination to overview tm-276 (#322)
    * feat(frontend): add pagination component tm-276
    
    * feat(frontend): add pagination types tm-276
    
    * fix(frontend): default pagination value tm-276
    
    * feat(frontend/shared): add pagination to overview page tm-276
    
    * feat(shared): pagination and user course validation schemas tm-276
    
    * feat(backend/shared): add paging to findByUser route tm-276
    
    * fix(frontend/backend/shared): quality criteria, increase default pagination page count tm-276
    
    * fix(frontend): fix imports and totalMyCoursesCount name tm-276
    
    * fix(shared): remove subfolders in enums and types tm-276
    
    * fix(frontend): move pagination logic into usePagination tm-276

--- apps/frontend/src/libs/hooks/hooks.ts
+++ apps/frontend/src/libs/hooks/hooks.ts
@@ -3,6 +3,7 @@ export { useAppForm } from "./use-app-form/use-app-form.hook.js";
 export { useAppSelector } from "./use-app-selector/use-app-selector.hook.js";
 export { useAppTitle } from "./use-app-titile/use-app-title.hooks.js";
 export { useHandleClickOutside } from "./use-handle-click-outside/use-handle-click-outside.hook.js";
+export { usePagination } from "./use-pagination/use-pagination.hook.js";
 export { useCallback, useEffect, useMemo, useRef, useState } from "react";
 export { useController as useFormController } from "react-hook-form";
 export { useLocation, useNavigate, useParams } from "react-router-dom";
--- apps/frontend/src/pages/overview/overview.tsx
+++ apps/frontend/src/pages/overview/overview.tsx
@@ -1,11 +1,12 @@
-import { Courses, Loader } from "~/libs/components/components.js";
-import { DataStatus } from "~/libs/enums/enums.js";
+import { Courses, Loader, Pagination } from "~/libs/components/components.js";
+import { DataStatus, PaginationValue } from "~/libs/enums/enums.js";
 import {
 	useAppDispatch,
 	useAppSelector,
 	useAppTitle,
 	useCallback,
 	useEffect,
+	usePagination,
 	useState,
 } from "~/libs/hooks/hooks.js";
 import { actions as userCourseActions } from "~/modules/user-courses/user-courses.js";
@@ -14,17 +15,25 @@ import { type UserAuthResponseDto } from "~/modules/users/users.js";
 import { AddCourseModal, WelcomeHeader } from "./libs/components/components.js";
 import styles from "./styles.module.css";
 
+const PAGINATION_PAGES_CUT_COUNT = 5;
+
 const Overview: React.FC = () => {
-	const { courses, isLoading, user } = useAppSelector((state) => {
+	const { courses, isLoading, totalCount, user } = useAppSelector((state) => {
 		return {
 			courses: state.userCourses.myCourses,
 			isLoading: state.userCourses.dataStatus === DataStatus.PENDING,
+			totalCount: state.userCourses.totalMyCoursesCount,
 			user: state.auth.user as UserAuthResponseDto,
 		};
 	});
 	const dispatch = useAppDispatch();
 	const [isAddCourseModalOpen, setIsAddCourseModalOpen] =
 		useState<boolean>(false);
+	const { handlePageChange, page, pages, pagesCount } = usePagination({
+		pageSize: PaginationValue.DEFAULT_COUNT,
+		pagesCutCount: PAGINATION_PAGES_CUT_COUNT,
+		totalCount,
+	});
 
 	const handleModalOpen = useCallback(() => {
 		setIsAddCourseModalOpen(true);
@@ -38,11 +47,13 @@ const Overview: React.FC = () => {
 	useEffect(() => {
 		void dispatch(
 			userCourseActions.loadMyCourses({
-				id: user.id,
+				count: PaginationValue.DEFAULT_COUNT,
+				page,
 				search: "",
+				userId: user.id,
 			}),
 		);
-	}, [dispatch, user]);
+	}, [dispatch, user, page]);
 
 	return (
 		<div className={styles["container"]}>
@@ -54,6 +65,12 @@ const Overview: React.FC = () => {
 				) : (
 					<Courses courses={courses} userId={user.id} />
 				)}
+				<Pagination
+					currentPage={page}
+					onPageChange={handlePageChange}
+					pages={pages}
+					pagesCount={pagesCount}
+				/>
 			</div>
 			{isAddCourseModalOpen && <AddCourseModal onClose={handleModalClose} />}
 		</div>
