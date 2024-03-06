1b5763c3db75962de22c2b8043a9487fc53437ea
Shadow <oleksandr.vpro@gmail.com>
2024-03-06 12:49:49 +0200

    refactor(frontend): change chat-socket middleware tm-113

--- apps/frontend/src/libs/modules/store/middlewares/chat-socket.middleware.ts
+++ apps/frontend/src/libs/modules/store/middlewares/chat-socket.middleware.ts
@@ -4,46 +4,45 @@ import {
 	isAction,
 } from "@reduxjs/toolkit";
 
-import {
-	SocketEvent,
-	SocketNamespace,
-	socket,
-} from "~/libs/modules/socket/socket.js";
-import { type AppDispatch } from "~/libs/types/types.js";
+import { SocketEvent, SocketNamespace } from "~/libs/modules/socket/socket.js";
+import { type ExtraArguments } from "~/libs/modules/store/store.js";
+import { type AppDispatch, type RootState } from "~/libs/types/types.js";
 import { type ChatMessageItemResponseDto } from "~/modules/chat-messages/chat-messages.js";
 import { actions as chatsActions } from "~/modules/chats/chats.js";
 
-const chatSocketInstance = socket.getInstance(SocketNamespace.CHAT);
+const chatSocket = ({
+	extra: { socket },
+}: {
+	extra: ExtraArguments;
+}): Middleware<unknown, RootState, AppDispatch> => {
+	const chatSocketInstance = socket.getInstance(SocketNamespace.CHAT);
 
-type SocketMiddlewareParameters = {
-	dispatch: AppDispatch;
-};
+	return ({ dispatch }) => {
+		chatSocketInstance.on(
+			SocketEvent.CHAT_ADD_NEW_MESSAGE,
+			(message: ChatMessageItemResponseDto) => {
+				void dispatch(chatsActions.addMessageToCurrentChat(message));
+			},
+		);
 
-const chatSocket: Middleware = ({ dispatch }: SocketMiddlewareParameters) => {
-	chatSocketInstance.on(
-		SocketEvent.CHAT_ADD_NEW_MESSAGE,
-		(message: ChatMessageItemResponseDto) => {
-			void dispatch(chatsActions.addMessageToCurrentChat(message));
-		},
-	);
+		return (next) => {
+			return (action) => {
+				if (isAction(action) && action.type === chatsActions.joinRoom.type) {
+					chatSocketInstance.emit(
+						SocketEvent.CHAT_JOIN_ROOM,
+						(action as PayloadAction<string>).payload,
+					);
+				}
 
-	return (next) => {
-		return (action) => {
-			if (isAction(action) && action.type === chatsActions.joinRoom.type) {
-				chatSocketInstance.emit(
-					SocketEvent.CHAT_JOIN_ROOM,
-					(action as PayloadAction<string>).payload,
-				);
-			}
+				if (isAction(action) && action.type === chatsActions.leaveRoom.type) {
+					chatSocketInstance.emit(
+						SocketEvent.CHAT_LEAVE_ROOM,
+						(action as PayloadAction<string>).payload,
+					);
+				}
 
-			if (isAction(action) && action.type === chatsActions.leaveRoom.type) {
-				chatSocketInstance.emit(
-					SocketEvent.CHAT_LEAVE_ROOM,
-					(action as PayloadAction<string>).payload,
-				);
-			}
-
-			return next(action);
+				return next(action);
+			};
 		};
 	};
 };
--- apps/frontend/src/libs/modules/store/store.module.ts
+++ apps/frontend/src/libs/modules/store/store.module.ts
@@ -8,6 +8,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import { AppEnvironment } from "~/libs/enums/enums.js";
 import { type Config } from "~/libs/modules/config/config.js";
 import { notification } from "~/libs/modules/notification/notification.js";
+import { socket } from "~/libs/modules/socket/socket.js";
 import { reducer as appReducer } from "~/libs/slices/app/app.js";
 import { authApi, reducer as authReducer } from "~/modules/auth/auth.js";
 import {
@@ -69,6 +70,7 @@ type ExtraArguments = {
 	friendsApi: typeof friendsApi;
 	notification: typeof notification;
 	sectionStatusApi: typeof sectionStatusApi;
+	socket: typeof socket;
 	storage: typeof storage;
 	userApi: typeof userApi;
 	userCourseApi: typeof userCourseApi;
@@ -92,7 +94,7 @@ class Store {
 					thunk: {
 						extraArgument: this.extraArguments,
 					},
-				}).prepend([handleError, chatSocket]);
+				}).prepend([handleError, chatSocket({ extra: this.extraArguments })]);
 			},
 			reducer: {
 				app: appReducer,
@@ -121,6 +123,7 @@ class Store {
 			friendsApi,
 			notification,
 			sectionStatusApi,
+			socket,
 			storage,
 			userApi,
 			userCourseApi,
@@ -129,4 +132,5 @@ class Store {
 	}
 }
 
+export { type ExtraArguments };
 export { Store };
