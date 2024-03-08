a223e79112ad0e05cfba61709cb513899824ee0e
Maxim <toper.one111@gmail.com>
2024-03-08 17:02:31 +0200

    feat(backend): add cascade deletion of chats and messages as well tm-402

--- apps/backend/src/db/migrations/20240308093120_add_user_cascade_deletion.ts
+++ apps/backend/src/db/migrations/20240308093120_add_user_cascade_deletion.ts
@@ -1,12 +1,17 @@
 import { type Knex } from "knex";
 
 const TableName = {
+	CHAT_MESSAGES: "chat_messages",
+	CHATS: "chats",
 	USER_DETAILS: "user_details",
 	USERS: "users",
 } as const;
 
 const ColumnName = {
+	FIRST_USER_ID: "first_user_id",
 	ID: "id",
+	SECOND_USER_ID: "second_user_id",
+	SENDER_USER_ID: "sender_user_id",
 	USER_ID: "user_id",
 } as const;
 
@@ -21,6 +26,29 @@ async function up(knex: Knex): Promise<void> {
 			.references(`${TableName.USERS}.${ColumnName.ID}`)
 			.onDelete(DELETE_STRATEGY);
 	});
+
+	await knex.schema.alterTable(TableName.CHATS, (table) => {
+		table.dropForeign(ColumnName.FIRST_USER_ID);
+		table.dropForeign(ColumnName.SECOND_USER_ID);
+
+		table
+			.foreign(ColumnName.FIRST_USER_ID)
+			.references(`${TableName.USERS}.${ColumnName.ID}`)
+			.onDelete(DELETE_STRATEGY);
+		table
+			.foreign(ColumnName.SECOND_USER_ID)
+			.references(`${TableName.USERS}.${ColumnName.ID}`)
+			.onDelete(DELETE_STRATEGY);
+	});
+
+	await knex.schema.alterTable(TableName.CHAT_MESSAGES, (table) => {
+		table.dropForeign(ColumnName.SENDER_USER_ID);
+
+		table
+			.foreign(ColumnName.SENDER_USER_ID)
+			.references(`${TableName.USERS}.${ColumnName.ID}`)
+			.onDelete(DELETE_STRATEGY);
+	});
 }
 
 async function down(knex: Knex): Promise<void> {
@@ -29,6 +57,26 @@ async function down(knex: Knex): Promise<void> {
 
 		table
 			.foreign(ColumnName.USER_ID)
+			.references(`${TableName.USERS}.${ColumnName.ID}`);
+	});
+
+	await knex.schema.alterTable(TableName.CHATS, (table) => {
+		table.dropForeign(ColumnName.FIRST_USER_ID);
+		table.dropForeign(ColumnName.SECOND_USER_ID);
+
+		table
+			.foreign(ColumnName.FIRST_USER_ID)
+			.references(`${TableName.USERS}.${ColumnName.ID}`);
+		table
+			.foreign(ColumnName.SECOND_USER_ID)
+			.references(`${TableName.USERS}.${ColumnName.ID}`);
+	});
+
+	await knex.schema.alterTable(TableName.CHAT_MESSAGES, (table) => {
+		table.dropForeign(ColumnName.SENDER_USER_ID);
+
+		table
+			.foreign(ColumnName.SENDER_USER_ID)
 			.references(`${TableName.USERS}.${ColumnName.ID}`);
 	});
 }
