2eabb5dc0488fd4b71fa1e53f1ba1059b340b6b6
Maxim <toper.one111@gmail.com>
2024-03-08 20:43:43 +0200

    chore(shared): update file names tm-388

--- packages/shared/src/libs/helpers/helpers.ts
+++ packages/shared/src/libs/helpers/helpers.ts
@@ -1,4 +1,4 @@
-export { checkIsDatePrecedesAnotherByOneDay } from "./check-is-date-precedes-another-by-one-day/check-is-date-precedes-another-by-one-day.js";
+export { checkIsDatePrecedesAnotherByOneDay } from "./check-is-date-precedes-another-by-one-day/check-is-date-precedes-another-by-one-day.helper.js";
 export { configureString } from "./configure-string/configure-string.helper.js";
 export { getDifferenceInHours } from "./get-difference-in-hours/get-difference-in-hours.helper.js";
 export { getFormattedDate } from "./get-formatted-date/get-formatted-date.helper.js";
