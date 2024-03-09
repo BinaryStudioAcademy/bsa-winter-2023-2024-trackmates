0796d9c30886fe57b51092c2bcc920ad82d64d30
Shadow <oleksandr.vpro@gmail.com>
2024-03-09 19:00:45 +0200

    feat(frontend): update component and its styles tm-288

--- apps/frontend/src/pages/notifications/notifications.tsx
+++ apps/frontend/src/pages/notifications/notifications.tsx
@@ -1,17 +1,33 @@
-import { Loader } from "~/libs/components/components.js";
-import { AppTitle, DataStatus } from "~/libs/enums/enums.js";
+import { Button, Loader } from "~/libs/components/components.js";
+import { AppRoute, AppTitle, DataStatus } from "~/libs/enums/enums.js";
+import { getValidClassNames } from "~/libs/helpers/helpers.js";
 import {
 	useAppDispatch,
 	useAppSelector,
 	useAppTitle,
 	useEffect,
+	useMemo,
+	useNavigate,
+	useSearchParams,
 } from "~/libs/hooks/hooks.js";
+import { type ValueOf } from "~/libs/types/types.js";
 import { actions } from "~/modules/user-notifications/user-notifications.js";
 
 import { NotificationList } from "./libs/components/notification-list/notification-list.js";
+import { NotificationFilter } from "./libs/enums/enums.js";
 import styles from "./styles.module.css";
 
 const Notifications: React.FC = () => {
+	const navigate = useNavigate();
+	const [queryParameters] = useSearchParams();
+	const notificationType = queryParameters.get("type") as ValueOf<
+		typeof NotificationFilter
+	> | null;
+
+	const possibleTypeValues = useMemo(() => {
+		return Object.values(NotificationFilter);
+	}, []);
+
 	const { isLoading, notifications } = useAppSelector(
 		({ userNotifications }) => {
 			return {
@@ -26,13 +42,47 @@ const Notifications: React.FC = () => {
 	useAppTitle(AppTitle.NOTIFICATIONS);
 
 	useEffect(() => {
-		void dispatch(actions.getUserNotifications());
-	}, [dispatch]);
+		const hasValidValue =
+			!notificationType || possibleTypeValues.includes(notificationType);
+
+		if (!hasValidValue) {
+			navigate(AppRoute.NOTIFICATIONS, { replace: true });
+
+			return;
+		}
+
+		void dispatch(actions.getUserNotifications(notificationType));
+	}, [dispatch, notificationType, navigate, possibleTypeValues]);
 
 	return (
 		<div className={styles["page"]}>
 			<div className={styles["content"]}>
-				<h2 className={styles["title"]}>Notifications</h2>
+				<div className={styles["header"]}>
+					<h2 className={styles["title"]}>Notification</h2>
+					<div className={styles["filters"]}>
+						{possibleTypeValues.map((filter) => {
+							const currentLink =
+								AppRoute.NOTIFICATIONS +
+								(filter === NotificationFilter.ALL ? "" : `?type=${filter}`);
+
+							const isActive =
+								notificationType === filter ||
+								(!notificationType && filter === NotificationFilter.ALL);
+
+							return (
+								<Button
+									className={getValidClassNames(
+										styles["filter-item"],
+										isActive && styles["active"],
+									)}
+									href={currentLink as ValueOf<typeof AppRoute>}
+									key={filter}
+									label={filter}
+								/>
+							);
+						})}
+					</div>
+				</div>
 				{isLoading ? (
 					<Loader color="orange" size="large" />
 				) : (
