40b753be7e9085a71bf320efb1343d86bd6f94d9
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2024-03-09 12:25:08 +0200

    feat(frontend): add a counter of unread messages to the header tm-395

--- apps/frontend/src/libs/components/header/header.tsx
+++ apps/frontend/src/libs/components/header/header.tsx
@@ -1,18 +1,23 @@
 import defaultAvatar from "~/assets/img/default-avatar.png";
 import { Button, Image, Link } from "~/libs/components/components.js";
-import { PAGES_WITH_SEARCH_BAR } from "~/libs/constants/constants.js";
+import {
+	EMPTY_ARRAY_LENGTH,
+	PAGES_WITH_SEARCH_BAR,
+} from "~/libs/constants/constants.js";
 import { AppRoute } from "~/libs/enums/enums.js";
 import { checkIfPathMatchingPattern } from "~/libs/helpers/helpers.js";
 import { useAppSelector, useLocation } from "~/libs/hooks/hooks.js";
 import { type UserAuthResponseDto } from "~/modules/users/users.js";
 
 import { SearchBar } from "../search-bar/search-bar.js";
+import { EMPTY_UNREAD_COUNT } from "./libs/constants.js";
 import styles from "./styles.module.css";
 
 const Header: React.FC = () => {
-	const { hasUnreadNotifications, notifications, user } = useAppSelector(
-		({ auth, userNotifications }) => {
+	const { chats, hasUnreadNotifications, notifications, user } = useAppSelector(
+		({ auth, chats, userNotifications }) => {
 			return {
+				chats: chats.chats,
 				hasUnreadNotifications: userNotifications.hasUnreadNotifications,
 				notifications: userNotifications.notifications,
 				user: auth.user as UserAuthResponseDto,
@@ -30,6 +35,12 @@ const Header: React.FC = () => {
 		return checkIfPathMatchingPattern(pathname, template);
 	});
 
+	const unreadChatsCount = chats.reduce((total, chat) => {
+		return total + Number(chat.unreadMessageCount);
+	}, EMPTY_UNREAD_COUNT);
+
+	const hasUnreadChats = unreadChatsCount > EMPTY_ARRAY_LENGTH;
+
 	return (
 		<header className={styles["header"]}>
 			<div className={styles["toolbar"]}>
@@ -40,13 +51,18 @@ const Header: React.FC = () => {
 					/>
 				)}
 				<nav className={styles["navigation"]}>
-					<Button
-						className={styles["icon-button"]}
-						hasVisuallyHiddenLabel
-						href={AppRoute.CHATS}
-						iconName="chats"
-						label="To chats"
-					/>
+					<div className={styles["chats-container"]}>
+						<Button
+							className={styles["icon-button"]}
+							hasVisuallyHiddenLabel
+							href={AppRoute.CHATS}
+							iconName="chats"
+							label="To chats"
+						/>
+						{hasUnreadChats && (
+							<span className={styles["chats-count"]}>{unreadChatsCount}</span>
+						)}
+					</div>
 					<div className={styles["notifications-container"]}>
 						<Button
 							className={styles["icon-button"]}
