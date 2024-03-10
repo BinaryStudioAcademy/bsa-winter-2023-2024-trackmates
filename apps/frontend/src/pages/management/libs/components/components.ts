1115804a7597113fc0622a394462486aa0d62772
TeaChan <valkova.tanya17@gmail.com>
2024-03-10 18:08:17 +0200

    feat(frontend): add management page tm-230

---
+++ apps/frontend/src/pages/management/libs/components/components.ts
@@ -0,0 +1,2 @@
+export { GroupsTab } from "./groups-tab/groups-tab.js";
+export { UsersTab } from "./users-tab/users-tab.js";
---
+++ apps/frontend/src/pages/management/libs/constants/links.constant.ts
@@ -0,0 +1,14 @@
+import { AppRoute } from "~/libs/enums/enums.js";
+
+const LINKS = [
+	{
+		title: "Users",
+		to: AppRoute.MANAGEMENT_USERS,
+	},
+	{
+		title: "Groups",
+		to: AppRoute.MANAGEMENT_GROUPS,
+	},
+];
+
+export { LINKS };
