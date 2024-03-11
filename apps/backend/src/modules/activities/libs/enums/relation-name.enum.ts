7f829fdb6230ab44d7e5e3576164f4981c75c97f
Ann Maksai <annamaksai99@gmail.com>
2024-03-11 08:13:41 +0200

    feat(backend): add users own activity tm-420 (#431)
    * feat(backend): add users own activity tm-420
    
    * fix(backend): fix error with groups tm-420
    
    ---------
    
    Co-authored-by: Farid Shabanov <61088675+fshabanov@users.noreply.github.com>

--- apps/backend/src/modules/activities/libs/enums/relation-name.enum.ts
+++ apps/backend/src/modules/activities/libs/enums/relation-name.enum.ts
@@ -1,7 +1,9 @@
 const RelationName = {
 	AVATAR_FILE: "avatarFile",
 	COMMENTS: "comments",
+	GROUPS: "groups",
 	LIKES: "likes",
+	PERMISSIONS: "permissions",
 	USER: "user",
 	USER_DETAILS: "userDetails",
 } as const;
