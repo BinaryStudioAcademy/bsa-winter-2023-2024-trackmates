876697810d643a5fd6048a45b2954a34d93e5ab5
Matvii <wantuchmatvii@gmail.com>
2024-03-11 15:21:07 +0200

    fix(frontend): add disabling button in each section tm-374

--- apps/frontend/src/modules/courses/slices/courses.slice.ts
+++ apps/frontend/src/modules/courses/slices/courses.slice.ts
@@ -2,11 +2,13 @@ import { createSlice } from "@reduxjs/toolkit";
 
 import { DataStatus } from "~/libs/enums/enums.js";
 import { type ValueOf } from "~/libs/types/types.js";
+import { actions as userCoursesActions } from "~/modules/user-courses/user-courses.js";
 
 import { type CourseDto } from "../libs/types/types.js";
 import { getAll, getById, getRecommended } from "./actions.js";
 
 type State = {
+	addedVendorCourseIds: string[];
 	currentCourse: CourseDto | null;
 	recommendedCourses: CourseDto[];
 	searchDataStatus: ValueOf<typeof DataStatus>;
@@ -14,6 +16,7 @@ type State = {
 };
 
 const initialState: State = {
+	addedVendorCourseIds: [],
 	currentCourse: null,
 	recommendedCourses: [],
 	searchDataStatus: DataStatus.IDLE,
@@ -52,6 +55,19 @@ const { actions, name, reducer } = createSlice({
 		builder.addCase(getRecommended.rejected, (state) => {
 			state.searchDataStatus = DataStatus.REJECTED;
 		});
+		builder.addCase(userCoursesActions.add.pending, (state, action) => {
+			state.addedVendorCourseIds = [
+				...state.addedVendorCourseIds,
+				action.meta.arg.vendorCourseId,
+			];
+		});
+		builder.addCase(userCoursesActions.add.rejected, (state, action) => {
+			state.addedVendorCourseIds = state.addedVendorCourseIds.filter(
+				(vendorCourseId) => {
+					return vendorCourseId != action.meta.arg.vendorCourseId;
+				},
+			);
+		});
 	},
 	initialState,
 	name: "courses",
@@ -59,6 +75,7 @@ const { actions, name, reducer } = createSlice({
 		clearCourses(state) {
 			state.recommendedCourses = [];
 			state.searchedCourses = [];
+			state.addedVendorCourseIds = [];
 		},
 	},
 });
