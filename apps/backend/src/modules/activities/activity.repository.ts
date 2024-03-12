643607fdaf5518df2ef6c03b522416bdff165bfa
Shadow <oleksandr.vpro@gmail.com>
2024-03-12 11:57:03 +0200

    feat(backend): update activity repository tm-446

--- apps/backend/src/modules/activities/activity.repository.ts
+++ apps/backend/src/modules/activities/activity.repository.ts
@@ -92,7 +92,7 @@ class ActivityRepository implements Repository<ActivityEntity> {
 			.select(`${DatabaseTableName.ACTIVITIES}.*`, this.getLikesCountQuery())
 			.select(`${DatabaseTableName.ACTIVITIES}.*`, this.getCommentsCountQuery())
 			.withGraphJoined(
-				`${RelationName.USER}.${RelationName.USER_DETAILS}.${RelationName.AVATAR_FILE}`,
+				`${RelationName.USER}.[${RelationName.USER_DETAILS}.${RelationName.AVATAR_FILE}, ${RelationName.GROUPS}.${RelationName.PERMISSIONS}]`,
 			)
 			.castTo<(ActivityModel & ActivityCounts) | undefined>()
 			.execute();
@@ -156,7 +156,7 @@ class ActivityRepository implements Repository<ActivityEntity> {
 					.where({ followerId: userId }),
 			)
 			.withGraphJoined(
-				`${RelationName.USER}.${RelationName.USER_DETAILS}.${RelationName.AVATAR_FILE}`,
+				`${RelationName.USER}.[${RelationName.USER_DETAILS}.${RelationName.AVATAR_FILE}, ${RelationName.GROUPS}.${RelationName.PERMISSIONS}]`,
 			)
 			.orderBy("updatedAt", SortOrder.DESC)
 			.castTo<(ActivityModel & ActivityCounts)[]>()
