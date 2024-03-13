4596c5adb5d9e745370494822126267860d4d282
TeaChan <valkova.tanya17@gmail.com>
2024-03-13 04:21:55 +0200

    feat(shared): add all-groups and all-permissions types tm-230

---
+++ packages/shared/src/modules/groups/libs/types/all-groups-response-dto.type.ts
@@ -0,0 +1,7 @@
+import { type GroupResponseDto } from "./group-response-dto.type.js";
+
+type AllGroupsResponseDto = {
+	items: GroupResponseDto[];
+};
+
+export { type AllGroupsResponseDto };
--- packages/shared/src/modules/groups/libs/types/types.ts
+++ packages/shared/src/modules/groups/libs/types/types.ts
@@ -1,2 +1,3 @@
+export { type AllGroupsResponseDto } from "./all-groups-response-dto.type.js";
 export { type GroupRequestDto } from "./group-request-dto.type.js";
 export { type GroupResponseDto } from "./group-response-dto.type.js";
