42b5f46d63cc2cfe01beb063a6d555195a88ffc4
Maxym Tsyganok <110524091+GEOFARL@users.noreply.github.com>
2024-03-14 11:58:06 +0200

    feat(frontend/backend/shared): Make "Udemy" the default vendor on the "Add Course" pop-up window tm-522 (#531)
    * feat(frontend): make only udemy the default vendor tm-522
    
    * refactor(frontend, backend, shared): create an enum for vendor keys tm-522

--- apps/backend/src/modules/vendors/vendor.model.ts
+++ apps/backend/src/modules/vendors/vendor.model.ts
@@ -2,9 +2,12 @@ import {
 	AbstractModel,
 	DatabaseTableName,
 } from "~/libs/modules/database/database.js";
+import { type ValueOf } from "~/libs/types/types.js";
+
+import { type VendorKey } from "./libs/enums/enums.js";
 
 class VendorModel extends AbstractModel {
-	public key!: string;
+	public key!: ValueOf<typeof VendorKey>;
 
 	public name!: string;
 
