10b1ddc8a36267420569063e69037addac6d3c66
Vfivew <hitori555111555@yahoo.com>
2024-03-14 16:23:56 +0200

    feat(frontend): back portal component tm-492

--- apps/frontend/src/libs/components/portal/portal.tsx
+++ apps/frontend/src/libs/components/portal/portal.tsx
@@ -1,11 +1,39 @@
 import { createPortal } from "react-dom";
 
+import { useEffect, useMemo } from "~/libs/hooks/hooks.js";
+
+import styles from "./styles.module.css";
+
 type Properties = {
 	children: React.ReactNode;
 };
 
+const NO_OVERFLOW_CLASS = styles["no-overflow"] as string;
+
 const Portal: React.FC<Properties> = ({ children }: Properties) => {
-	return createPortal(children, document.body);
+	const portalContainer = useMemo(() => {
+		const element = document.createElement("div");
+		element.classList.add(styles["portal"] as string);
+
+		return element;
+	}, []);
+
+	useEffect(() => {
+		const wasOverflowHidden =
+			document.body.classList.contains(NO_OVERFLOW_CLASS);
+		document.body.append(portalContainer);
+		document.body.classList.add(NO_OVERFLOW_CLASS);
+
+		return () => {
+			portalContainer.remove();
+
+			if (!wasOverflowHidden) {
+				document.body.classList.remove(NO_OVERFLOW_CLASS);
+			}
+		};
+	}, [portalContainer]);
+
+	return createPortal(children, portalContainer);
 };
 
 export { Portal };
