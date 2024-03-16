304c071d59e22bd53c3d063f02a02b49c87494cf
Vfivew <hitori555111555@yahoo.com>
2024-03-16 09:08:54 +0200

    feat(frontend): rename helper tm-502

--- apps/frontend/src/libs/hooks/use-pagination/libs/helpers/helpers.ts
+++ apps/frontend/src/libs/hooks/use-pagination/libs/helpers/helpers.ts
@@ -1,3 +1,3 @@
-export { checkIsValidPage } from "./check-is-valid-page/check-is-valid-page.helpers.js";
+export { checkIsValidPage } from "./check-is-valid-page/check-is-valid-page.helper.js";
 export { getPagesCut } from "./get-pages-cut/get-pages-cut.helper.js";
 export { getPagesRange } from "./get-pages-range/get-pages-range.helper.js";
