da97fcd53c22cb258110b1c989f639a0dfb98de8
Shadow <oleksandr.vpro@gmail.com>
2024-03-18 11:55:20 +0200

    refactor(backend): update order by statement tm-231

--- apps/backend/src/modules/courses/course.repository.ts
+++ apps/backend/src/modules/courses/course.repository.ts
@@ -165,7 +165,7 @@ class CourseRepository implements Repository<CourseEntity> {
 		const courses = await this.courseModel
 			.query()
 			.withGraphFetched("vendor")
-			.orderBy("id", "ASC")
+			.orderBy("id", SortOrder.ASC)
 			.execute();
 
 		return courses.map((course) => {
