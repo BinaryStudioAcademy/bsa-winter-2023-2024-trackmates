1d917f1792df07fa10d5bc297aee71ca58d77d11
Shadow <oleksandr.vpro@gmail.com>
2024-03-20 07:41:14 +0200

    feat(frontend): add Table component, its styles, enums and helpers tm-542

---
+++ apps/frontend/src/libs/components/table/libs/helpers/get-cell-properties.helper.ts
@@ -0,0 +1,28 @@
+import { type Cell, type TableCellProps } from "react-table";
+
+import { type ValueOf } from "~/libs/types/types.js";
+
+import { TableColumnAlign } from "../enums/enums.js";
+
+const getCellProperties = <T extends Record<string, unknown>>(
+	cell: Cell<T, unknown>,
+	columnAlign: Record<string, ValueOf<typeof TableColumnAlign>>,
+): TableCellProps => {
+	return {
+		...cell.getCellProps((properties) => {
+			return [
+				properties,
+				{
+					style: {
+						display: "flex",
+						justifyContent:
+							columnAlign[cell.column.Header?.toString() ?? ""] ??
+							TableColumnAlign.CENTER,
+					},
+				},
+			];
+		}),
+	};
+};
+
+export { getCellProperties };
