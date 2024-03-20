9fdd82ade65d7c0737a8d9ff61758dc3dde78031
Oleg Verhusha <91093431+tarmiel@users.noreply.github.com>
2024-03-20 15:19:42 +0200

    feat(frontend/backend): add possibility to delete own comments tm-489 (#599)
    * feat(frontend): add deletion of comment on frontend tm-489
    
    * feat(backend): add deletion of comment on backend tm-489
    
    * fix(backend): fix deleting comments under own activity  tm-489
    
    * fix(backend): fix updated controller params api tm-489
    
    * fix(backend): fix import permission error  tm-489
    
    * fix(backend): make check hook more generic tm-489
    
    * fix(frontend): split activities and comments logic tm-489
    
    * fix(backend): rename isEqual in hook tm-489
    
    * fix(backend/shared): add valid check and style fix tm-489

--- apps/backend/src/libs/types/types.ts
+++ apps/backend/src/libs/types/types.ts
@@ -2,6 +2,7 @@ export { type Entity } from "./entity.type.js";
 export { type Repository } from "./repository.type.js";
 export { type Service } from "./service.type.js";
 export {
+	type ExtractPath,
 	type PaginationRequestDto,
 	type PaginationResponseDto,
 	type ServerCommonErrorResponse,
