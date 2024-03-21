c1db49376b213f1634ec601905e05d382dfae84f
Vfivew <hitori555111555@yahoo.com>
2024-03-21 18:55:32 +0200

    fix(frontend): is toogle instead if else tm-651

--- apps/frontend/src/libs/hooks/use-toggle-scroll/use-toggle-scroll.ts
+++ apps/frontend/src/libs/hooks/use-toggle-scroll/use-toggle-scroll.ts
@@ -2,11 +2,7 @@ import { useEffect } from "~/libs/hooks/hooks.js";
 
 const useToggleScroll = (isOpen: boolean): void => {
 	useEffect(() => {
-		if (isOpen) {
-			document.body.classList.add("no-overflow");
-		} else {
-			document.body.classList.remove("no-overflow");
-		}
+		document.body.classList.toggle("no-overflow", isOpen);
 	}, [isOpen]);
 };
 
