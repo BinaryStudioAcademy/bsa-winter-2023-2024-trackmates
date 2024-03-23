b44cc1c218a6b280648c35e627c5f0ef6a511f64
Shadow <oleksandr.vpro@gmail.com>
2024-03-23 09:44:59 +0200

    refactor(backend): remove redundant type from repository type tm-645

--- apps/backend/src/libs/types/repository.type.ts
+++ apps/backend/src/libs/types/repository.type.ts
@@ -1,10 +1,8 @@
-import { type PaginationResponseDto } from "~/libs/types/types.js";
-
 type Repository<T = unknown> = {
 	create(payload: unknown): Promise<T>;
 	delete(id: number): Promise<boolean>;
 	find(id: number): Promise<T | null>;
-	findAll(payload: unknown): Promise<{ items: T[] } | PaginationResponseDto<T>>;
+	findAll(payload: unknown): Promise<{ items: T[] }>;
 	update(id: number, payload: unknown): Promise<T | null>;
 };
 
